b650805c5d6562626700c74fa59280fce4428b84
Felix Schnellert <completabledev@gmail.com>
2021-11-18 00:41:00 +0100

    Added itembuilder

--- src/main/java/eu/minevalley/core/api/misc/ItemBuilder.java
+++ src/main/java/eu/minevalley/core/api/misc/ItemBuilder.java
@@ -1,4 +1,21 @@
 package eu.minevalley.core.api.misc;
 
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+
 public interface ItemBuilder {
+
+    ItemBuilder setDisplayName(String displayName);
+
+    ItemBuilder setLore(String... lore);
+
+    ItemBuilder setDurability(short durability);
+
+    ItemBuilder setAmount(int amount);
+
+    ItemStack getItemStack();
+
+    ItemMeta getItemMeta();
+
+    ItemStack build();
 }
