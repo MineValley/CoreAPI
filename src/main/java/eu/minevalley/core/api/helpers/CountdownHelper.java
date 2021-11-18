2e26262dcdd7e2f323eb4c43769e777c4fb0fc1f
Felix Schnellert <completabledev@gmail.com>
2021-11-18 17:29:03 +0100

    added countdowns

--- src/main/java/eu/minevalley/core/api/helpers/CountdownHelper.java
+++ src/main/java/eu/minevalley/core/api/helpers/CountdownHelper.java
@@ -1,10 +1,13 @@
 package eu.minevalley.core.api.helpers;
 
 import eu.minevalley.core.api.utils.Countdown;
+import org.bukkit.scheduler.BukkitTask;
 
 public interface CountdownHelper {
 
     void startCountdown(Countdown countdown);
 
     void stopCountdown(Countdown countdown);
+
+    BukkitTask getTask();
 }
