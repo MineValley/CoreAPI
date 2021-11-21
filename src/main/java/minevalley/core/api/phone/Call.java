f3cfe2beb0b4ff11a84e9f46f4d8323afabcb237
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2021-11-21 05:52:24 +0100

    added getBegin()-method

--- src/main/java/minevalley/core/api/phone/Call.java
+++ src/main/java/minevalley/core/api/phone/Call.java
@@ -1,21 +1,32 @@
 package minevalley.core.api.phone;
 
+import java.time.Instant;
+
 public interface Call {
 
     /**
-     * Ends the current call
+     * Ends the current call.
      */
     void end();
 
     /**
-     * Gets the phone, that started the call
+     * Gets the phone, that started the call.
+     *
      * @return phone-object of caller
      */
     Phone getCaller();
 
     /**
-     * Gets the phone, that got called
+     * Gets the phone, that got called.
+     *
      * @return phone-object of called player
      */
     Phone getPartner();
+
+    /**
+     * Gets the time at which the call was started.
+     *
+     * @return time as instant-object
+     */
+    Instant getBegin();
 }
