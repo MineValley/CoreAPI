0599db3ab9bc0b4317874f5e9eb027856c847890
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2021-12-07 21:28:19 +0100

    Added some useful methods

--- src/main/java/minevalley/core/api/regions/Area.java
+++ src/main/java/minevalley/core/api/regions/Area.java
@@ -1,4 +1,19 @@
 package minevalley.core.api.regions;
 
+import org.bukkit.Location;
+
 public interface Area {
+
+    /**
+     * Gets the first location that defines this area.
+     * @return first location of this area
+     */
+    Location getFirstLocation();
+
+    /**
+     * Gets the second location that defines this area.
+     * @return second location of this area
+     */
+    Location getSecondLocation();
+
 }
--- src/main/java/minevalley/core/api/regions/Region.java
+++ src/main/java/minevalley/core/api/regions/Region.java
@@ -1,5 +1,8 @@
 package minevalley.core.api.regions;
 
+import org.bukkit.Chunk;
+import org.bukkit.entity.Player;
+
 import java.util.List;
 
 public interface Region {
@@ -9,4 +12,22 @@ public interface Region {
      * @return list of areas
      */
     List<Area> getAreas();
+
+    /**
+     * Gets this regions id.
+     * @return id as integer
+     */
+    int getId();
+
+    /**
+     * Gets a list with the players that are currently in this region.
+     * @return list of players
+     */
+    Player[] getPlayers();
+
+    /**
+     * Gets a list with the chunks that this region lies on.
+     * @return list of chunks
+     */
+    Chunk[] getChunks();
 }
