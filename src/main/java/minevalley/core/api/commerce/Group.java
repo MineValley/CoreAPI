2af9bba051adc63174696bb51a3fe921c49c2648
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-01-16 01:26:02 +0100

    Changed return of setName()

--- src/main/java/minevalley/core/api/commerce/Group.java
+++ src/main/java/minevalley/core/api/commerce/Group.java
@@ -15,10 +15,10 @@ public interface Group {
 
     /**
      * Sets the name of this group.
-     * <p>
-     * <b>Note:</b> Always check, if the name is already used!
+     *
+     * @return Result of the rename
      */
-    void setName();
+    RenameFeedback setName();
 
     /**
      * Gets the id of this group.
@@ -36,25 +36,38 @@ public interface Group {
 
     /**
      * Gets a list with the members of this group.
+     *
      * @return members as list
      */
     List<Member> getMembers();
 
     /**
      * Removes the specific member from this group.
+     *
      * @param member member to remove
      */
     void removeMember(Member member);
 
     /**
      * Removes the member with the specific unique id from this group.
+     *
      * @param uniqueId member to remove
      */
     void removeMember(String uniqueId);
 
     /**
      * Adds a member with the specific unique id to this group.
+     *
      * @param uniqueId member to add
      */
     void addMember(String uniqueId);
+
+    enum RenameFeedback {
+        FORBIDDEN_CHARACTERS,
+        FORBIDDEN_WORDS,
+        ALREADY_USED,
+        TOO_LONG,
+        TOO_SHORT,
+        SUCCESS
+    }
 }
