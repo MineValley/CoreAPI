ebe2a990e5ebed3cbeda6badc6bb14af80f25cab
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-01-17 22:14:15 +0100

    Made some changes on methods

--- src/main/java/minevalley/core/api/corporations/Task.java
+++ src/main/java/minevalley/core/api/corporations/Task.java
@@ -6,13 +6,27 @@ public interface Task {
      * Gets the number of possible repetitions of this task.
      * @return number of possible repetitions (-1 if there is no restriction)
      */
-    int getNumberOfRepetitions();
+    int getMaximumAmountOfCompletions();
 
     /**
-     * Sets the number of possible repetitions.
-     * -1 if there is no restriction!
+     * Defines how often this task can be completed (-1 for no restriction).
+     * <b>Note: </b> This must not be less than the number of tasks already completed!
+     * @param completions amount of possible completions
      */
-    void setNumberOfRepetitions(int repetitions);
+    void setMaximumAmountOfCompletions(int completions);
+
+    /**
+     * Gets how often this task is completed already.
+     * @return amount of completions as integer
+     */
+    int getCurrentAmountOfCompletions();
+
+    /**
+     * Defines how often this task has already been completed.
+     * <b>Note: </b> This cannot be higher than the number of possible completions!
+     * @param completions amount of completions so far
+     */
+    void setCurrentAmountOfCompletions(int completions);
 
     /**
      * Gets the wages paid to the user who completes this task.
