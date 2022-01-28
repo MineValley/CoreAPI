4141fa05f5797fd96fe87751012aab4126f26af2
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-01-28 08:50:30 +0100

    Added getLocality() method

--- src/main/java/minevalley/core/api/regions/Locality.java
+++ src/main/java/minevalley/core/api/regions/Locality.java
@@ -1,22 +1,35 @@
 package minevalley.core.api.regions;
 
+import org.bukkit.Chunk;
+
 public interface Locality {
 
     /**
      * Gets this localitys id.
+     *
      * @return id as integer
      */
     int getId();
 
     /**
      * Gets the name of this locality.
+     *
      * @return name as string
      */
     String getName();
 
     /**
      * Gets the description of this locality.
+     *
      * @return descripton as string
      */
     String getDescription();
+
+    /**
+     * Gets the locality connected with the specific chunk.
+     *
+     * @param chunk chunk to get locality from
+     * @return specific chunk's locality
+     */
+    Locality getLocality(Chunk chunk);
 }
