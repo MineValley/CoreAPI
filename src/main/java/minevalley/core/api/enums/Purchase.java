0593998c93a781aebfd8a289f2db52e3096f1c36
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-03-27 13:05:40 +0200

    added mc version and purchase

---
+++ src/main/java/minevalley/core/api/enums/Purchase.java
@@ -0,0 +1,14 @@
+package minevalley.core.api.enums;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum Purchase {
+    PREMIUM
+
+    // More coming soon...
+
+}
