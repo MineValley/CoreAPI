8591ed695103311dafdbd7b0b8b7fde7a35c132d
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-04-24 22:36:44 +0200

    added setResult()

--- src/main/java/minevalley/core/api/crafting/CraftingProcess.java
+++ src/main/java/minevalley/core/api/crafting/CraftingProcess.java
@@ -2,11 +2,17 @@ package minevalley.core.api.crafting;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 import minevalley.core.api.User;
+import org.bukkit.inventory.ItemStack;
 
 @Getter
 @RequiredArgsConstructor
 public class CraftingProcess {
-    final User user;
-    final Recipe recipe;
+
+    private final User user;
+    private final Recipe recipe;
+
+    @Setter
+    private ItemStack result = recipe.getResult();
 }
