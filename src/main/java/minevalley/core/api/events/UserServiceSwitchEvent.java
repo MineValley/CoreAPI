8518953ebbea5d57f4ee2f8d80aa930bc169c66d
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-06-20 20:55:44 +0200

    added userserviceswitchevent

---
+++ src/main/java/minevalley/core/api/events/UserServiceSwitchEvent.java
@@ -0,0 +1,41 @@
+package minevalley.core.api.events;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import minevalley.core.api.User;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+@RequiredArgsConstructor
+@Getter
+public final class UserServiceSwitchEvent extends Event {
+
+    public static final HandlerList HANDLER_LIST = new HandlerList();
+
+    private final User user;
+    private final ServiceSwitch serviceSwitch;
+
+    public static HandlerList getHandlerList() {
+        return HANDLER_LIST;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLER_LIST;
+    }
+
+    public enum ServiceSwitch {
+        JOIN_TEAM_SERVICE,
+        LEAVE_TEAM_SERVICE,
+        JOIN_SUPPORT_SERVICE,
+        LEAVE_SUPPORT_SERVICE,
+        JOIN_MEDIC_SERVICE,
+        LEAVE_MEDIC_SERVICE,
+        JOIN_POLICE_SERVICE,
+        LEAVE_POLICE_SERVICE,
+        JOIN_MAIL_SERVICE,
+        LEAVE_MAIL_SERVICE,
+        JOIN_DISPOSAL_SERVICE,
+        LEAVE_DISPOSAL_SERVICE;
+    }
+}
