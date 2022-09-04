b642baa190ccd85d7e62e6162b9425be0cce1a2e
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2022-09-04 20:45:21 +0200

    added names to debug types

--- src/main/java/minevalley/core/api/enums/DebugType.java
+++ src/main/java/minevalley/core/api/enums/DebugType.java
@@ -1,12 +1,20 @@
 package minevalley.core.api.enums;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum DebugType {
-    SYSTEM,
-    DATABASE,
-    SETTINGS,
-    STATISTICS,
-    POLICE,
-    MEDIC,
-    USER_ACTION,
-    MONEY
+    SYSTEM("System"),
+    DATABASE("Datenbank"),
+    SETTINGS("Spielereinstellungen"),
+    STATISTICS("Statistik"),
+    POLICE("Polizei"),
+    MEDIC("Rettungsdienst"),
+    USER_ACTION("Spieleraktionen"),
+    FINANCE("Finanzen");
+
+    private final String name;
 }
