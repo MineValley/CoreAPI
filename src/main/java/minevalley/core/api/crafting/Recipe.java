abad341f9e33f4f469372b62f99c3ae3b7e5b98f
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2023-05-25 16:18:02 +0200

    took use Map instead of HashMap

--- src/main/java/minevalley/core/api/crafting/Recipe.java
+++ src/main/java/minevalley/core/api/crafting/Recipe.java
@@ -3,28 +3,34 @@ package minevalley.core.api.crafting;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public interface Recipe {
 
-    HashMap<Integer, RecipeItem> ingredients = new HashMap<>();
+    Map<Integer, RecipeItem> ingredients = new HashMap<>();
 
     /**
      * Set a recipe name
+     *
      * @return Recipe name
      */
     String recipeName();
 
     /**
      * Defines the target(output) item
+     *
      * @return Output Item
      */
     ItemStack outputItem();
 
     /**
      * Defines the ingredients of the recipe with an integer which is used to map the items on
+     *
      * @return list with ingredients
      */
-    default HashMap<Integer, RecipeItem> ingredients() {return ingredients;}
+    default Map<Integer, RecipeItem> ingredients() {
+        return ingredients;
+    }
 
     /**
      * Defines how many power levels the craft process should take
@@ -33,9 +39,11 @@ public interface Recipe {
 
     /**
      * Defines if the recipe can be crafted in a normal crafting table
+     *
      * @return isVanillaCraftable - standard = false due 5x5 crafting
      */
-     default boolean isVanillaCraftable() {return false;}
-
+    default boolean isVanillaCraftable() {
+        return false;
+    }
 
 }
