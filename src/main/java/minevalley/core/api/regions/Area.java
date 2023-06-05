57153ecb82dbfbb1d8acc2cba086068773ba2528
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2023-06-05 09:26:32 +0200

    reworked areas

--- src/main/java/minevalley/core/api/regions/Area.java
+++ src/main/java/minevalley/core/api/regions/Area.java
@@ -1,5 +1,6 @@
 package minevalley.core.api.regions;
 
+import org.bukkit.Location;
 import org.bukkit.block.Block;
 
 import java.util.List;
@@ -11,22 +12,33 @@ public interface Area {
      *
      * @return first location of this area
      */
+    @Deprecated
     Block getFirstLocation();
+
+    Block getMinBlock();
 
     /**
      * Gets the second location that defines this area.
      *
      * @return second location of this area
      */
+    @Deprecated
     Block getSecondLocation();
+
+    Block getMaxBlock();
 
     List<Block> getBlocks();
 
     boolean contains(Block block);
 
+    default boolean contains(Location location) {
+        return contains(location.getBlock());
+    }
+
     /**
      * Converts this area to a string
      * @return string that represents this area
      */
+    @Deprecated
     String toString();
 }
