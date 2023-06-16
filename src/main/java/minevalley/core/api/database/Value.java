2f89e7f7c3f794f329b4b9fb7cd19424f784b681
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2023-06-16 03:03:21 +0200

    optimized code

--- src/main/java/minevalley/core/api/database/Value.java
+++ src/main/java/minevalley/core/api/database/Value.java
@@ -1,13 +1,13 @@
 package minevalley.core.api.database;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-@RequiredArgsConstructor
 @Getter
+@AllArgsConstructor
 public final class Value {
 
     private final String column;
-
     private final Object value;
 }
