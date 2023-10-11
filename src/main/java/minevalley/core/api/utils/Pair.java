c15c23328e2001b24cba02372051955bb36accad
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2023-10-11 05:49:09 +0200

    simplified code

--- src/main/java/minevalley/core/api/utils/Pair.java
+++ src/main/java/minevalley/core/api/utils/Pair.java
@@ -1,27 +1,13 @@
 package minevalley.core.api.utils;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
 public class Pair<K, V> {
     private K key;
     private V value;
-
-    public Pair(K key, V value) {
-        this.key = key;
-        this.value = value;
-    }
-
-    public void setKey(K key) {
-        this.key = key;
-    }
-
-    public K getKey() {
-        return key;
-    }
-
-    public void setValue(V value) {
-        this.value = value;
-    }
-
-    public V getValue() {
-        return value;
-    }
 }
