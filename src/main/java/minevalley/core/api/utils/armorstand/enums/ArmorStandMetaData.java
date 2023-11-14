e5bcfbbce951ec2e9627900a14783be60a109420
_JXc <hsvbuh12@gmail.com>
2023-11-14 12:27:45 +0100

    updated armorstand components

--- src/main/java/minevalley/core/api/utils/armorstand/enums/ArmorStandMetaData.java
+++ src/main/java/minevalley/core/api/utils/armorstand/enums/ArmorStandMetaData.java
@@ -5,8 +5,7 @@ public enum ArmorStandMetaData {
     IS_SMALL(0x01),
     HAS_ARMS(0x04),
     HAS_BASE_PLATE(0x08),
-    IS_VISIBLE(0x10);
-
+    IS_MARKER(0x10);
     ArmorStandMetaData(int bit) {}
 
 }
