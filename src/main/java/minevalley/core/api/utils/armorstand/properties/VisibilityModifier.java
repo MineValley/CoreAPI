28c2625d09289a5be58aaf971ab4529dbb2671ac
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2023-11-15 09:44:37 +0100

    reworked visibility modifier

--- src/main/java/minevalley/core/api/utils/armorstand/properties/VisibilityModifier.java
+++ src/main/java/minevalley/core/api/utils/armorstand/properties/VisibilityModifier.java
@@ -1,14 +1,38 @@
 package minevalley.core.api.utils.armorstand.properties;
 
 import minevalley.core.api.users.OnlineUser;
-import org.bukkit.entity.Player;
 
 import java.util.List;
 
 public interface VisibilityModifier {
 
-    void hideFromPlayers(OnlineUser... onlineUsers);
-    void exposeToPlayers(OnlineUser... onlineUsers);
+    /**
+     * The armorstand will only be visible to players in this list.
+     * <br>
+     * <b>Note 1:</b> If list is empty, armorstand is visible to everyone!
+     * <br>
+     * <b>Note 2:</b> The viewers list can be overwritten by the hide list!
+     */
+    List<OnlineUser> getViewersList();
 
-    List<OnlineUser> getVisibilityModifierPlayers();
+    void addToViewersList(OnlineUser... onlineUsers);
+
+    void removeFromViewersList(OnlineUser... onlineUsers);
+
+    /**
+     * <b>Emptying this list results in everyone seeing the armorstand (If not part of hide list)</b>
+     */
+    void emptyViewersList();
+
+    /**
+     * The armorstand will explicitly hide from the players in this list, even if the viewers list contains them (or is empty)!
+     */
+    List<OnlineUser> getHideList();
+
+    void addToHideList(OnlineUser... onlineUsers);
+
+    void removeFromHideList(OnlineUser... onlineUsers);
+
+    void emptyHideList();
+
 }
