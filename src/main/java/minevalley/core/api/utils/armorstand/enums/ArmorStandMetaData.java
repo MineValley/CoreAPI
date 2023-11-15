6aea34263a2580500bb8fd662695ffb4ad344274
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2023-11-15 09:24:08 +0100

    Optimized enum structure

--- src/main/java/minevalley/core/api/utils/armorstand/enums/ArmorStandMetaData.java
+++ src/main/java/minevalley/core/api/utils/armorstand/enums/ArmorStandMetaData.java
@@ -1,11 +1,17 @@
 package minevalley.core.api.utils.armorstand.enums;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum ArmorStandMetaData {
 
     IS_SMALL(0x01),
     HAS_ARMS(0x04),
     HAS_BASE_PLATE(0x08),
     IS_MARKER(0x10);
-    ArmorStandMetaData(int bit) {}
 
+    private final int masking;
 }
