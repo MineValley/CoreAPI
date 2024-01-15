1d5f5bddff3421cee8c77ffdcbd9e715c8f1b8b3
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2024-01-15 04:15:55 +0100

    added @return to getPages()-javadoc

--- src/main/java/minevalley/core/api/mail/Letter.java
+++ src/main/java/minevalley/core/api/mail/Letter.java
@@ -4,6 +4,8 @@ public interface Letter extends Parcel {
 
     /**
      * Gets the content of this letter.
+     *
+     * @return content of each page in this letter.
      */
     String[] getPages();
 
