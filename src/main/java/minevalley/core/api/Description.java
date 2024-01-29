fa79734a16f4df97e8c1abbc1803b977ca6052cc
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2024-01-29 02:24:54 +0100

    add @Target to Description annotation

--- src/main/java/minevalley/core/api/Description.java
+++ src/main/java/minevalley/core/api/Description.java
@@ -2,8 +2,10 @@ package minevalley.core.api;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.RUNTIME)
+@Target(java.lang.annotation.ElementType.TYPE)
 public @interface Description {
 
     String name();
