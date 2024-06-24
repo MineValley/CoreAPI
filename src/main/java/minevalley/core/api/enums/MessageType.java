587eb4d7f76b8b8005daecac80462a30b7083b7c
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2024-06-24 03:38:42 +0200

    remove deprecated message types

--- src/main/java/minevalley/core/api/enums/MessageType.java
+++ src/main/java/minevalley/core/api/enums/MessageType.java
@@ -28,42 +28,6 @@ public enum MessageType {
     INFO_BW(" §f" + CustomCharacter.MV_BW + " §8━ §7"),
 
     /**
-     * Used to display any information to the user. Uses a turquoise-colored "i"-letter.
-     */
-    @Deprecated
-    INFO_TURQUOISE("§bℹ §7"),
-
-    /**
-     * Used to display any information to the user. Uses an aqua-colored "i"-letter.
-     */
-    @Deprecated
-    INFO_AQUA("§3ℹ §7"),
-
-    /**
-     * Used to display any information to the user. Uses a yellow-colored "i"-letter.
-     */
-    @Deprecated
-    INFO_YELLOW("§eℹ §7"),
-
-    /**
-     * Used to display any information to the user. Uses a gold-colored "i"-letter.
-     */
-    @Deprecated
-    INFO_GOLD("§6ℹ §7"),
-
-    /**
-     * Used to display any information to the user. Uses a red-colored "i"-letter.
-     */
-    @Deprecated
-    INFO_RED("§cℹ §7"),
-
-    /**
-     * Used to display any information to the user. Uses a pink-colored "i"-letter.
-     */
-    @Deprecated
-    INFO_PINK("§dℹ §7"),
-
-    /**
      * Used to signal the success of a process or action, by using the action-bar.
      */
     ACTION_BAR_SUCCESS("§a✔§8 ━ §7"),
