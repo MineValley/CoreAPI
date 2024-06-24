ff7b17d518bc874faf7af1f5c27fe15873afd446
Vincent B <79211348+Snabeldier@users.noreply.github.com>
2024-06-24 16:57:48 +0200

    feat(fake-armorstands): add new visibility system for fake-armorstands (#184)

--- src/main/java/minevalley/core/api/armorstand/FakeArmorStand.java
+++ src/main/java/minevalley/core/api/armorstand/FakeArmorStand.java
@@ -4,14 +4,15 @@ import minevalley.core.api.armorstand.modifiers.*;
 import minevalley.core.api.users.OnlineUser;
 
 import java.util.function.BiConsumer;
+import java.util.function.Function;
 
 public interface FakeArmorStand extends EquipmentModifier, LocationModifier, MetadataModifier, PassengerModifier,
-        PoseModifier, RotationModifier, VisibilityModifier {
+        PoseModifier, RotationModifier {
 
     /**
-     * get the custom id from the armorstand
+     * Get the custom id from the armorstand
      *
-     * @return int
+     * @return id as int
      */
     int getId();
 
@@ -22,6 +23,8 @@ public interface FakeArmorStand extends EquipmentModifier, LocationModifier, Met
 
     /**
      * Destroys/de-spawn the armorstand
+     * <br>
+     * <b>Note:</b> Armorstand can be simply respawned with {@link #spawn()}
      */
     void destroy();
 
@@ -41,9 +44,23 @@ public interface FakeArmorStand extends EquipmentModifier, LocationModifier, Met
      */
     void setVisibilityRange(int visibilityRange);
 
+    /**
+     * Defines the action when a player interacts with the armorstand
+     *
+     * @param consumer is called whenever a user interacts with the armorstand
+     */
     void onClick(BiConsumer<OnlineUser, InteractType> consumer);
 
+    /**
+     * Defines which users can see the armorstand
+     * <br>
+     * <b>Default:</b> Everyone can see the armorstand
+     *
+     * @param function defines whether a specific user can see the armorstand
+     */
+    void isSeeing(Function<OnlineUser, Boolean> function);
+
     enum InteractType {
-        LEFT_CLICK, RIGHT_CLICK;
+        LEFT_CLICK, RIGHT_CLICK
     }
 }
