0ae0431f639ce75d23d72340b0f17cb77fc7711a
Thorben B. <thorbenbunger@gmail.com>
2024-07-06 11:07:36 +0200

    feat: add TrashCanInteraction enum

---
+++ src/main/java/minevalley/core/api/trashcans/enums/TrashCanInteraction.java
@@ -0,0 +1,16 @@
+package minevalley.core.api.trashcans.enums;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum TrashCanInteraction {
+
+    ADD_ITEM,
+    CLEAR,
+    REMOVE_ITEM,
+    CLICK,
+
+}
