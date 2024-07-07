af7cfed1967dcbbdb6192c409e3adb9d512e75e8
Vincent B <79211348+Snabeldier@users.noreply.github.com>
2024-07-07 03:12:18 +0200

    feat(fake-blocks): add new visibility system to fake blocks (#200)

--- src/main/java/minevalley/core/api/regions/utils/FakeBlock.java
+++ src/main/java/minevalley/core/api/regions/utils/FakeBlock.java
@@ -1,35 +1,24 @@
 package minevalley.core.api.regions.utils;
 
 import minevalley.core.api.users.OnlineUser;
-import org.bukkit.entity.Player;
+
+import java.util.function.Function;
 
 public interface FakeBlock {
 
     /**
-     * Shows the block to specific players.
+     * Defines which users can see the fake block
+     * <br>
+     * <b>Default:</b> Everyone can see the fake block
      *
-     * @param players Players to whom the block is shown
+     * @param function defines whether a specific user can see the fake block
      */
-    void show(Player... players);
+    void setVisibility(Function<OnlineUser, Boolean> function);
 
     /**
-     * Hides the block from specific players.
-     *
-     * @param players Players from whom the block is hidden
+     * Updates the visibility of the fake block
+     * <br>
+     * <b>Note:</b> This simply calls the function defined in {@link #setVisibility(Function)} for every user that the fake block is or could be visible to
      */
-    void hide(Player... players);
-
-    /**
-     * Shows the block to specific users.
-     *
-     * @param users Users to whom the block is shown
-     */
-    void show(OnlineUser... users);
-
-    /**
-     * Hides the block from specific users.
-     *
-     * @param users Users from whom the block is hidden
-     */
-    void hide(OnlineUser... users);
+    void updateVisibility();
 }
