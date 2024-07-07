2cad53970c7ca5e6dd620e9fa2f368988f54891f
Thorben B. <thorbenbunger@gmail.com>
2024-07-07 17:08:34 +0200

    refactor: add setter to classes

--- src/main/java/minevalley/core/api/trashcans/events/UserAddItemTrashCanEvent.java
+++ src/main/java/minevalley/core/api/trashcans/events/UserAddItemTrashCanEvent.java
@@ -1,14 +1,17 @@
 package minevalley.core.api.trashcans.events;
 
 import lombok.Getter;
+import lombok.Setter;
 import minevalley.core.api.users.User;
 import org.bukkit.inventory.ItemStack;
 
-/** The {@code UserAddItemTrashCanEvent} is triggered when a user adds an item to a trash can.
- *  This event is only triggered when the action is allowed by the system, for example, when the trash can is not full.
+/**
+ * The {@code UserAddItemTrashCanEvent} is triggered when a user adds an item to a trash can.
+ * This event is only triggered when the action is allowed by the system, for example, when the trash can is not full.
  *
  * @see UserInteractTrashCanEvent
  */
+@Setter
 @Getter
 public class UserAddItemTrashCanEvent extends UserInteractTrashCanEvent {
 
@@ -16,19 +19,12 @@ public class UserAddItemTrashCanEvent extends UserInteractTrashCanEvent {
 
     /**
      * Constructor for {@code UserAddItemTrashCanEvent.}
-     * @param user the user performing the interaction
+     *
+     * @param user      the user performing the interaction
      * @param itemStack the added item
      */
     public UserAddItemTrashCanEvent(User user, ItemStack itemStack) {
         super(user);
-        this.itemStack = itemStack;
-    }
-
-    /**
-     * Changes the item stack that was added to the trash can.
-     * @param itemStack the new item stack
-     */
-    public void setItemStack(ItemStack itemStack) {
         this.itemStack = itemStack;
     }
 }
--- src/main/java/minevalley/core/api/trashcans/events/UserRemoveItemTrashCanEvent.java
+++ src/main/java/minevalley/core/api/trashcans/events/UserRemoveItemTrashCanEvent.java
@@ -1,14 +1,17 @@
 package minevalley.core.api.trashcans.events;
 
 import lombok.Getter;
+import lombok.Setter;
 import minevalley.core.api.users.User;
 import org.bukkit.inventory.ItemStack;
 
-/** The {@code UserRemoveItemTrashCanEvent} is triggered when a user removes an item from a trash can.
- *  This event is only triggered when the action is allowed by the system, for example, when the trash can is not empty.
+/**
+ * The {@code UserRemoveItemTrashCanEvent} is triggered when a user removes an item from a trash can.
+ * This event is only triggered when the action is allowed by the system, for example, when the trash can is not empty.
  *
  * @see UserInteractTrashCanEvent
  */
+@Setter
 @Getter
 public class UserRemoveItemTrashCanEvent extends UserInteractTrashCanEvent {
 
@@ -16,19 +19,12 @@ public class UserRemoveItemTrashCanEvent extends UserInteractTrashCanEvent {
 
     /**
      * Constructor for {@code UserRemoveItemTrashCanEvent.}
-     * @param user the user performing the interaction
+     *
+     * @param user      the user performing the interaction
      * @param itemStack the removed item
      */
     public UserRemoveItemTrashCanEvent(User user, ItemStack itemStack) {
         super(user);
-        this.itemStack = itemStack;
-    }
-
-    /**
-     * Changes the item stack that was removed from the trash can.
-     * @param itemStack the new item stack
-     */
-    public void setItemStack(ItemStack itemStack) {
         this.itemStack = itemStack;
     }
 }
