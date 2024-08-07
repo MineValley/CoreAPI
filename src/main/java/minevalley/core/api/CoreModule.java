c78c6e27bc810053ef715baa64c3fcefe54f58a2
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2024-08-08 01:23:37 +0200

    add constructor to CoreModule

--- src/main/java/minevalley/core/api/CoreModule.java
+++ src/main/java/minevalley/core/api/CoreModule.java
@@ -2,6 +2,16 @@ package minevalley.core.api;
 
 public abstract class CoreModule {
 
+    private final Description description;
+
+    public CoreModule() {
+        final Class<? extends CoreModule> clazz = getClass();
+        if (!clazz.isAnnotationPresent(Description.class)) {
+            throw new IllegalArgumentException("Description annotation is missing in module '" + clazz.getSimpleName() + "'");
+        }
+        this.description = clazz.getAnnotation(Description.class);
+    }
+
     /**
      * Is called when the module is enabled.
      */
