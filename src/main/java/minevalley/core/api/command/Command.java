b651f691a1950cbb623a26f528d1fd88a806ae7b
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2024-08-08 01:34:47 +0200

    removal of CommandOptions

--- src/main/java/minevalley/core/api/command/Command.java
+++ src/main/java/minevalley/core/api/command/Command.java
@@ -1,48 +1,70 @@
 package minevalley.core.api.command;
 
-import minevalley.core.api.users.OnlineUser;
+import lombok.NonNull;
 
-import java.util.List;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
-/**
- * This interface corresponds to the often used CommandExecutor-interface. It's used very similarly, but has some advantages.
- */
-public interface Command {
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
+public @interface Command {
 
     /**
-     * This is the equivalent to the onCommand-method of the CommandExecutor-interface
+     * This list is used to make all the strings usable command-variations. It should contain each alias and the original spelling of the command
      *
-     * @param user user who executed this command
-     * @param args array of arguments
-     * @return response-enum that decides what is sent to the user
+     * @return array of all command varies and its aliases
      */
-    CommandResponse onCommand(OnlineUser user, String[] args);
+    @NonNull
+    String[] commands();
 
     /**
-     * This method is used to define the completions displayed when the player presses TAB.
-     *
-     * @param user user who pressed TAB
-     * @param args array of the arguments
-     * @return list of the completions to display
-     */
-    List<String> onTabComplete(OnlineUser user, String[] args);
-
-    /**
-     * This method is used to define whether a user is permissioned to execute/tab this command.
-     * This is checked, when a user tabs or executes the command.
      * <p>
-     * <b>Note:</b> This method should not have any side effects!
-     *
-     * @param user user who tries to execute/tab the command
-     * @return true, if the user is permissioned - false, if he isn't (error-message will be sent automatically)
+     * This string defines the correct syntax.
+     * </p>
+     * Begin with the mostly used alias of the command itself without "/"
+     * Then add all possible arguments with "&lt; &gt;"
+     * arguments that are not required are marked with "[&lt; &gt;]"
+     * <br>
+     * Different argument suggestions are separated by vertical bars (example: <i>see below</i>).
+     * <br>
+     * Literal argument suggestions are written in quotation marks (e. g. /restart &lt;seconds | 'now'&gt;)
      */
-    boolean isPermissioned(OnlineUser user);
+    @NonNull
+    String syntax() default "<command>";
 
-    enum CommandResponse {
-        SUCCESS,
-        ABUSE_WARNING,
-        WRONG_SYNTAX,
-        NO_PERMISSION,
-        NONE
-    }
+    /**
+     * This message defined here will be sent, when the onCommand-method returns SUCCESS.
+     * <br>
+     * Don't use this method, if the player shouldn't get printed any message on success.
+     */
+    String successMessage() default "";
+
+    /**
+     * If this is enabled, users that are allowed to use this command are also allowed to tab it.
+     * Disable this if you want to make custom permission-checks beyond the command-options!
+     *
+     * @return true, if everyone with permission can tab this command.
+     */
+    boolean allowTabbing() default true;
+
+    /**
+     * If abuseWarning() is true, the team will be notified if a player tries to execute this command without permission
+     * The return of ABUSE_WARNING in the onCommand method is not related to this setting and can also be carried out if nothing (or false) is specified here
+     *
+     * @return true, if the team should be notified about illegal use of this command.
+     */
+    boolean abuseWarning() default false;
+
+    /**
+     * If a value is specified here, the user must execute the command with the specified number of arguments.
+     * Otherwise, the command will not be invoked and the user will receive a syntax error.
+     */
+    int correctSyntaxLength() default -1;
+
+    /**
+     * If executableIfFrozen is true, this command can be executed even though the user is currently frozen by a team member.
+     */
+    boolean executableIfFrozen() default false;
 }
