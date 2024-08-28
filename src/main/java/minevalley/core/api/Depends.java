ccaa69e309d39da3f7c1c2230ced32b7f9df0111
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2024-08-28 14:06:00 +0200

    add annotations

--- src/main/java/minevalley/core/api/Depends.java
+++ src/main/java/minevalley/core/api/Depends.java
@@ -1,5 +1,12 @@
 package minevalley.core.api;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE_USE)
 public @interface Depends {
 
     /**
