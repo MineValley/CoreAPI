d395cb103ec09eb23944d40ce4681e394f6a152a
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-01-11 00:53:15 +0100

    add javadoc to team

--- src/main/java/minevalley/core/api/team/Team.java
+++ src/main/java/minevalley/core/api/team/Team.java
@@ -8,7 +8,20 @@ import javax.annotation.Nonnull;
 @SuppressWarnings("unused")
 public interface Team extends MessageReceiver {
 
+    /**
+     * Send a message to the team chat.
+     *
+     * @param message The message to send.
+     * @throws IllegalArgumentException If the message is null.
+     */
     void sendTeamChatMessage(@Nonnull String message) throws IllegalArgumentException;
 
+    /**
+     * Send a message to the team chat.
+     *
+     * @param message     The message to send.
+     * @param instruction The instruction to send.
+     * @throws IllegalArgumentException If the message or instruction is null.
+     */
     void sendTeamChatMessage(@Nonnull String message, @Nonnull Instruction instruction) throws IllegalArgumentException;
 }
