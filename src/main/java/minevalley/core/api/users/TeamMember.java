0d4a55889ebe641e73f7b59693632c9ad907fcab
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-01-14 03:34:47 +0100

    add exceptions

---
+++ src/main/java/minevalley/core/api/users/TeamMember.java
@@ -0,0 +1,4 @@
+package minevalley.core.api.users.friends;
+
+public interface TeamMember {
+}
---
+++ src/main/java/minevalley/core/api/users/exceptions/UserNotPermittedException.java
@@ -0,0 +1,22 @@
+package minevalley.core.api.users.exceptions;
+
+import lombok.Getter;
+import minevalley.core.api.users.User;
+
+import javax.annotation.Nonnull;
+
+@Getter
+public class UserNotPermittedException extends RuntimeException {
+
+    private final User user;
+
+    public UserNotPermittedException(@Nonnull User user) {
+        super("User " + user.getName() + " is not permitted to perform this action.");
+        this.user = user;
+    }
+
+    public UserNotPermittedException(@Nonnull User user, @Nonnull String message) {
+        super(message);
+        this.user = user;
+    }
+}
