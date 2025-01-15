a09d4c4c1639ae3c6226d7261dec47f427acd63e
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-01-15 16:21:56 +0100

    add Server interface

--- src/main/java/minevalley/core/api/server/Server.java
+++ src/main/java/minevalley/core/api/server/Server.java
@@ -1,6 +1,55 @@
 package minevalley.core.api.server;
 
-public interface Server {
+import minevalley.core.api.chat.MessageReceiver;
 
+import javax.annotation.Nonnull;
 
+@SuppressWarnings("unused")
+public interface Server extends MessageReceiver {
+
+    /**
+     * Get the server type.
+     *
+     * @return the server type
+     */
+    @Nonnull
+    ServerType getType();
+
+    /**
+     * Gets whether the server is in development mode.
+     *
+     * @return true, if the server is in development mode
+     */
+    boolean isInDevelopmentMode();
+
+    /**
+     * Gets whether the server is in maintenance mode.
+     *
+     * @return true, if the server is in maintenance mode
+     */
+    boolean isInMaintenanceMode();
+
+    /**
+     * Sets the server to maintenance mode.
+     *
+     * @param maintenance true, if the server should be in maintenance mode
+     */
+    void setMaintenanceMode(boolean maintenance);
+
+    /**
+     * Gets whether the server is fully started.
+     *
+     * @return true, if the server is fully started
+     */
+    boolean isFullyStarted();
+
+    /**
+     * Restarts the server.
+     */
+    void restart();
+
+    /**
+     * Restarts the server and marks the cleaning routine to be executed on start up.
+     */
+    void restartWithCleaningRoutine();
 }
