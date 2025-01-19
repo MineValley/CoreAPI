9ed5c99ce88fb07201e5e59337d175573b6bdc74
Vincent B. <79211348+Snabeldier@users.noreply.github.com>
2025-01-19 02:53:02 +0100

    chore: remove accept() from FillItem (#282)

--- src/main/java/minevalley/core/api/gui/FillItem.java
+++ src/main/java/minevalley/core/api/gui/FillItem.java
@@ -1,9 +1,5 @@
 package minevalley.core.api.gui;
 
-import minevalley.core.api.Core;
-import minevalley.core.api.users.OnlineUser;
-import org.bukkit.entity.Player;
-import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 
 import javax.annotation.Nonnull;
@@ -14,12 +10,5 @@ public record FillItem(@Nonnull ItemStack itemStack, @Nullable Consumer<GuiItemC
 
     public FillItem {
         if (itemStack == null) throw new IllegalArgumentException("ItemStack cannot be null");
-    }
-
-    @SuppressWarnings("unused")
-    public void accept(final @Nonnull InventoryClickEvent event) {
-        if (!(event.getWhoClicked() instanceof Player player)) return;
-        final OnlineUser user = Core.getOnlineUser(player);
-        if (callback != null) callback.accept(new GuiItemClick(user, event.getClick(), event.getAction()));
     }
 }
