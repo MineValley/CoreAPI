e317606c7f1cccac342f868046f527dd4773084d
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-02-05 18:01:26 +0100

    add some methods to fakeblock

--- src/main/java/minevalley/core/api/utils/FakeBlock.java
+++ src/main/java/minevalley/core/api/utils/FakeBlock.java
@@ -1,6 +1,27 @@
 package minevalley.core.api.utils;
 
 import minevalley.core.api.modifiers.VisibilityModifier;
+import org.bukkit.Material;
+import org.bukkit.block.Block;
 
+import javax.annotation.Nonnull;
+
+@SuppressWarnings("unused")
 public interface FakeBlock extends VisibilityModifier {
+
+    /**
+     * Get the block this fake block is placed on
+     *
+     * @return the block this fake block is placed on
+     */
+    @Nonnull
+    Block getBlock();
+
+    /**
+     * Get the material of this fake block
+     *
+     * @return the material of this fake block
+     */
+    @Nonnull
+    Material getMaterial();
 }
