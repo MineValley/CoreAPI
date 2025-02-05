ff59923e0a9a65d2ee340967795347395f8b1c6c
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-02-05 17:30:38 +0100

    add javadoc

--- src/main/java/minevalley/core/api/utils/Hologram.java
+++ src/main/java/minevalley/core/api/utils/Hologram.java
@@ -6,21 +6,57 @@ import minevalley.core.api.modifiers.VisibilityModifier;
 @SuppressWarnings("unused")
 public interface Hologram extends LocationModifier, VisibilityModifier {
 
+    /**
+     * Returns the lines of the hologram.
+     *
+     * @return the lines of the hologram.
+     */
     String[] getLines();
 
+    /**
+     * Returns the line at the specified index.
+     *
+     * @param line the index of the line.
+     * @return the line at the specified index.
+     */
     default String getLine(int line) {
         return getLines()[line];
     }
 
+    /**
+     * Adds a line to the hologram.
+     *
+     * @param text the text of the line.
+     */
     default void addLine(String text) {
         addLineAfter(getLines().length - 1, text);
     }
 
+    /**
+     * Adds a line to the hologram at the specified index.
+     *
+     * @param line the index of the line.
+     * @param text the text of the line.
+     */
     void addLineAfter(int line, String text);
 
+    /**
+     * Changes the line at the specified index.
+     *
+     * @param line the index of the line.
+     * @param text the new text of the line.
+     */
     void changeLine(int line, String text);
 
+    /**
+     * Removes the line at the specified index.
+     *
+     * @param line the index of the line.
+     */
     void removeLine(int line);
 
+    /**
+     * Removes the hologram.
+     */
     void delete();
 }
