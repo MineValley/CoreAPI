2b743f4337c6b2abd486d2562c405776565d9708
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-02-05 18:22:45 +0100

    add annotations and excecptions

--- src/main/java/minevalley/core/api/utils/Hologram.java
+++ src/main/java/minevalley/core/api/utils/Hologram.java
@@ -3,6 +3,9 @@ package minevalley.core.api.utils;
 import minevalley.core.api.modifiers.LocationModifier;
 import minevalley.core.api.modifiers.VisibilityModifier;
 
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+
 @SuppressWarnings("unused")
 public interface Hologram extends LocationModifier, VisibilityModifier {
 
@@ -11,6 +14,7 @@ public interface Hologram extends LocationModifier, VisibilityModifier {
      *
      * @return the lines of the hologram.
      */
+    @Nonnull
     String[] getLines();
 
     /**
@@ -18,8 +22,9 @@ public interface Hologram extends LocationModifier, VisibilityModifier {
      *
      * @param line the index of the line.
      * @return the line at the specified index.
+     * @throws IndexOutOfBoundsException if the line is out of bounds.
      */
-    default String getLine(int line) {
+    default String getLine(@Nonnegative int line) throws IndexOutOfBoundsException {
         return getLines()[line];
     }
 
@@ -27,8 +32,10 @@ public interface Hologram extends LocationModifier, VisibilityModifier {
      * Adds a line to the hologram.
      *
      * @param text the text of the line.
+     * @throws IllegalArgumentException if the text is null.
      */
-    default void addLine(String text) {
+    default void addLine(@Nonnull String text) throws IllegalArgumentException {
+        if (text == null) throw new IllegalArgumentException("The text cannot be null.");
         addLineAfter(getLines().length - 1, text);
     }
 
@@ -37,21 +44,26 @@ public interface Hologram extends LocationModifier, VisibilityModifier {
      *
      * @param line the index of the line.
      * @param text the text of the line.
+     * @throws IndexOutOfBoundsException if the line is out of bounds.
+     * @throws IllegalArgumentException  if the text is null.
      */
-    void addLineAfter(int line, String text);
+    void addLineAfter(@Nonnegative int line, @Nonnull String text) throws IndexOutOfBoundsException, IllegalArgumentException;
 
     /**
      * Changes the line at the specified index.
      *
      * @param line the index of the line.
      * @param text the new text of the line.
+     * @throws IndexOutOfBoundsException if the line is out of bounds.
+     * @throws IllegalArgumentException  if the text is null.
      */
-    void changeLine(int line, String text);
+    void changeLine(@Nonnegative int line, @Nonnull String text) throws IndexOutOfBoundsException, IllegalArgumentException;
 
     /**
      * Removes the line at the specified index.
      *
      * @param line the index of the line.
+     * @throws IndexOutOfBoundsException if the line is out of bounds.
      */
-    void removeLine(int line);
+    void removeLine(@Nonnegative int line) throws IndexOutOfBoundsException;
 }
