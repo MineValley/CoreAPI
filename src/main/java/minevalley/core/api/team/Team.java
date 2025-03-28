a8c2b923067b02d7a6701fdfbabfdaf7ca6f2c4d
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-03-28 18:20:26 +0100

    feat: add component support for team chat

--- src/main/java/minevalley/core/api/team/Team.java
+++ src/main/java/minevalley/core/api/team/Team.java
@@ -3,6 +3,7 @@ package minevalley.core.api.team;
 import minevalley.core.api.audio.SoundReceiver;
 import minevalley.core.api.messaging.MessageReceiver;
 import minevalley.core.api.messaging.instruction.Instruction;
+import net.kyori.adventure.text.ComponentLike;
 
 import javax.annotation.Nonnull;
 
@@ -25,4 +26,21 @@ public interface Team extends MessageReceiver, SoundReceiver {
      * @throws IllegalArgumentException If the message or instruction is null.
      */
     void sendTeamChatMessage(@Nonnull String message, @Nonnull Instruction instruction) throws IllegalArgumentException;
+
+    /**
+     * Send a message to the team chat.
+     *
+     * @param message the message to send
+     * @throws IllegalArgumentException if the message is null
+     */
+    void sendTeamChatMessage(@Nonnull ComponentLike message) throws IllegalArgumentException;
+
+    /**
+     * Sends a message to the team chat.
+     *
+     * @param message     the message to send
+     * @param instruction the instruction to send
+     * @throws IllegalArgumentException if the message or instruction is null
+     */
+    void sendTeamChatMessage(@Nonnull ComponentLike message, @Nonnull Instruction instruction) throws IllegalArgumentException;
 }
