09f2ee62debecd3a24e4802c1f15f9cb8dc43289
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-04-12 18:26:08 +0200

    chore: replace collection with array

--- src/main/java/minevalley/core/api/commands/CommandLiteral.java
+++ src/main/java/minevalley/core/api/commands/CommandLiteral.java
@@ -3,8 +3,6 @@ package minevalley.core.api.commands;
 import org.jetbrains.annotations.Contract;
 
 import javax.annotation.Nonnull;
-import java.util.Collection;
-import java.util.Collections;
 
 /**
  * A command literal is a string that represents a (sub-) command.
@@ -32,13 +30,13 @@ public interface CommandLiteral extends CommandBuilder<CommandLiteral> {
      * Register the command and rule the access via the requirements if needed.
      * </li>
      * <li>
-     * If you want to set the command to be executable when the user is frozen, use {@link #register(boolean)}.
+     * If you want to set the command to be executable when the user is frozen, use {@link #register(boolean, String...)}.
      * </li>
      * </ul>
      * </p>
      */
     default void register() {
-        register(false, Collections.emptyList());
+        register(false);
     }
 
     /**
@@ -49,21 +47,8 @@ public interface CommandLiteral extends CommandBuilder<CommandLiteral> {
      * </p>
      *
      * @param executableIfFrozen sets whether the command can be executed when the user is frozen.
-     */
-    default void register(boolean executableIfFrozen) {
-        register(executableIfFrozen, Collections.emptyList());
-    }
-
-    /**
-     * Registers the command so it is accessible to the users.
-     * <p>
-     * <b>Note:</b> Registering a command should always be done directly after the module initialization. Registering a command inside a scheduler or after some other kind of action (other commands, events...) is not possible.
-     * Register the command and rule the access via the requirements if needed.
-     * </p>
-     *
-     * @param executableIfFrozen sets whether the command can be executed when the user is frozen.
-     * @param aliases            the aliases of the command.
+     * @param aliases            the aliases of the command (can be left empty).
      * @throws IllegalArgumentException if the aliases are null.
      */
-    void register(boolean executableIfFrozen, @Nonnull Collection<String> aliases) throws IllegalArgumentException;
+    void register(boolean executableIfFrozen, @Nonnull String... aliases) throws IllegalArgumentException;
 }
