b8d654200dfc2a174b4b90f1d9f3249048d20600
Vincent B. <79211348+Snabeldier@users.noreply.github.com>
2025-04-19 23:56:34 +0200

    feat: introduce ProxyUser (#363)

--- src/main/java/minevalley/core/api/users/OnlineUser.java
+++ src/main/java/minevalley/core/api/users/OnlineUser.java
@@ -1,17 +1,7 @@
 package minevalley.core.api.users;
 
-import minevalley.core.api.audio.SoundReceiver;
 import minevalley.core.api.economy.AccountUser;
 import minevalley.core.api.economy.BankAccount;
-import minevalley.core.api.messaging.DialogReceiver;
-import minevalley.core.api.messaging.MessageReceiver;
-import minevalley.core.api.regions.utils.PlayerLocation;
-import minevalley.core.api.users.enums.Fraction;
-import minevalley.core.api.users.enums.McVersion;
-import minevalley.core.api.users.enums.TabListView;
-import minevalley.core.api.users.exceptions.UserNotPermittedException;
-import minevalley.core.api.utils.ChatHandler;
-import minevalley.core.api.utils.ClickableMessage;
 import minevalley.core.api.vehicles.LoadedVehicle;
 import minevalley.core.api.vehicles.VehicleManager;
 import org.bukkit.Location;
@@ -19,17 +9,15 @@ import org.bukkit.block.Block;
 import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
 import org.jetbrains.annotations.Contract;
 
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 import javax.annotation.Nullable;
-import java.time.Duration;
 import java.util.function.Consumer;
 
 @SuppressWarnings("unused")
-public interface OnlineUser extends User, DialogReceiver, MessageReceiver, SoundReceiver {
+public interface OnlineUser extends ProxyUser {
 
     /**
      * Gets the player object of this user.
@@ -40,56 +28,11 @@ public interface OnlineUser extends User, DialogReceiver, MessageReceiver, Sound
     Player player();
 
     /**
-     * Gets if the player has any type of team-rank.
-     *
-     * @return true, if the user is part of the server-team (and in team-service)
-     */
-    @Contract(pure = true)
-    boolean isTeamler();
-
-    /**
-     * Gets the team-member object of this user.
-     *
-     * @return team-member object
-     * @throws UserNotPermittedException if the user is no team-member
-     * @see #isTeamler()
-     */
-    @Nonnull
-    TeamMember team() throws UserNotPermittedException;
-
-    /**
-     * Joins the team-service.
-     *
-     * @throws UnsupportedOperationException if the user is not allowed to join the team-service
-     * @throws IllegalStateException         if the user is already in a service
-     */
-    void joinTeamService() throws UnsupportedOperationException, IllegalStateException;
-
-    /**
      * Closes the inventory of this user.
      */
     default void closeInventory() {
         player().closeInventory();
     }
-
-    /**
-     * Gets whether this user is currently logged in via labymod.
-     * <br>
-     * <b>Note:</b> When joining, this boolean isn't set immediately. Make sure to use a delay, when using this method in a PlayerJoinListener!
-     *
-     * @return true, if this user is using labymod
-     */
-    @Contract(pure = true)
-    boolean isUsingLabyMod();
-
-    /**
-     * Gets the version of the client this user is using.
-     *
-     * @return client version
-     */
-    @Nonnull
-    @Contract(pure = true)
-    McVersion getVersion();
 
     /**
      * Update the list of commands sent to the client.
@@ -123,23 +66,6 @@ public interface OnlineUser extends User, DialogReceiver, MessageReceiver, Sound
     void sendCredits();
 
     /**
-     * Sets the tab-list view of this user.
-     *
-     * @param view tab-list view to be set
-     * @throws IllegalArgumentException if view is null
-     */
-    void setTabListView(@Nonnull TabListView view) throws IllegalArgumentException;
-
-    /**
-     * Gets the current tab-list view of this user.
-     *
-     * @return current tab-list view
-     */
-    @Nonnull
-    @Contract(pure = true)
-    TabListView getTabListView();
-
-    /**
      * Asks the user which bank account he wants to use (e.g. to pay sth)
      * He can choose from every bank account he's permissioned to transfer money from.
      * If he chooses nothing, the callback isn't called.
@@ -152,35 +78,6 @@ public interface OnlineUser extends User, DialogReceiver, MessageReceiver, Sound
                            @Nonnull AccountUser.BankAccountUserPermission... requiredPermissions)
             throws IllegalArgumentException;
 
-    // FractionService
-
-    /**
-     * Gets the user's fraction.
-     * Note: The user must be in fraction-service, otherwise this is NONE
-     *
-     * @return users fraction
-     */
-    @Contract(pure = true)
-    Fraction getFractionService();
-
-    /**
-     * Sets the user's current fraction service.
-     *
-     * @param service fraction to enter service
-     * @throws IllegalArgumentException  if the service is null
-     * @throws UserNotPermittedException if the user is not allowed to enter the service
-     * @throws IllegalStateException     if the user is already in a service
-     */
-    void enterFractionService(@Nonnull Fraction service) throws IllegalArgumentException, UserNotPermittedException,
-            IllegalStateException;
-
-    /**
-     * Lets the user leave the fraction-service.
-     *
-     * @throws IllegalStateException if the user is not in a service
-     */
-    void leaveFractionService() throws IllegalStateException;
-
     /**
      * Sets the user's health to the maximum.
      *
@@ -190,80 +87,59 @@ public interface OnlineUser extends User, DialogReceiver, MessageReceiver, Sound
     int heal();
 
     /**
-     * Revives the user if he's dead.
-     *
-     * @throws IllegalStateException if the user is not dead
-     */
-    void revive() throws IllegalStateException;
-
-    /**
-     * Gets whether the user has reached the maximum idle time and is marked as afk.
-     *
-     * @return true, if the user reached the maximum idle time
-     */
-    @Contract(pure = true)
-    boolean isIdle();
-
-    /**
-     * Gets whether this user is vanished (only team members can be vanished).
-     *
-     * @return true, if this user is vanished
-     */
-    @Contract(pure = true)
-    boolean isVanish();
-
-    /**
-     * Sets whether this user is vanished.
-     *
-     * @param vanish true, if this user should be vanished
-     * @throws UnsupportedOperationException if the user is not allowed to vanish
-     */
-    void setVanish(boolean vanish) throws UnsupportedOperationException;
-
-    /**
-     * Imprisons this user for the given duration.
-     *
-     * @param duration duration of the imprisonment
-     * @throws UnsupportedOperationException if the user cannot be imprisoned (e.g. because he's a team-member)
-     * @throws IllegalStateException         if the user is already in prison
-     */
-    void imprison(@Nonnull Duration duration) throws UnsupportedOperationException, IllegalStateException;
-
-    /**
-     * Releases this user from prison.
-     *
-     * @throws IllegalStateException if the user is not in prison
-     */
-    void releaseFromPrison() throws IllegalStateException;
-
-    /**
-     * Gets whether the user has completed registration on our network.
-     *
-     * @return true, if the user is registered
-     */
-    @Contract(pure = true)
-    boolean isRegistered();
-
-    /**
      * Gets whether this user is allowed to break/place/use a block at the given position regarding the regions-system.
      *
      * @param block block to check
      * @return true, if this user is allowed to break/place/use a block here
      */
     @Contract(pure = true)
-    boolean isAllowedToUse(Block block);
+    boolean isAllowedToUse(@Nonnull Block block);
 
+    /**
+     * Send a sign change to this user.
+     *
+     * @param block sign to change
+     * @param line1 first line
+     * @param line2 second line
+     * @param line3 third line
+     * @param line4 fourth line
+     * @throws IllegalArgumentException if the sign is null or not a sign.
+     */
     void changeSign(@Nonnull Block block, @Nullable String line1, @Nullable String line2, @Nullable String line3,
                     @Nullable String line4) throws IllegalArgumentException;
 
+    /**
+     * Resets a previously changed sign to its original state.
+     *
+     * @param block sign to reset
+     * @throws IllegalArgumentException if the sign is null or not a sign.
+     */
     void resetSign(@Nonnull Block block) throws IllegalArgumentException;
 
+    /**
+     * Sets this users navigation target.
+     *
+     * @param location location to set the target to
+     * @throws IllegalArgumentException if the location is null
+     */
     void setNavigationTarget(@Nonnull Location location) throws IllegalArgumentException;
 
+    /**
+     * Resets the navigation target.
+     */
     void resetNavigationTarget();
 
+    /**
+     * Sets the latest join to now.
+     */
     void updateLatestJoin();
 
+    /**
+     * Gets the vehicle this user is in.
+     *
+     * @return vehicle the user is in
+     */
+    @Nullable
     @Contract(pure = true)
     default LoadedVehicle getVehicle() {
         final Entity vehicle = player().getVehicle();
@@ -271,55 +147,6 @@ public interface OnlineUser extends User, DialogReceiver, MessageReceiver, Sound
         if (!(vehicle instanceof ArmorStand)) return null;
         return VehicleManager.getVehicle((ArmorStand) vehicle);
     }
-
-    void checkRegistration();
-
-    default void chat(@Nonnull ChatType type, @Nonnull String message) throws IllegalArgumentException {
-        if (type == null) throw new IllegalArgumentException("Type cannot be null");
-        ChatHandler.chat(this, type, message);
-    }
-
-    default void chat(@Nonnull String message) throws IllegalArgumentException {
-        if (message == null) throw new IllegalArgumentException("Message cannot be null");
-        ChatHandler.chat(this, ChatType.NORMAL, message);
-    }
-
-    void performCommand(@Nonnull String command) throws IllegalArgumentException;
-
-    boolean addItem(@Nonnull ItemStack item) throws IllegalArgumentException;
-
-    @Contract(pure = true)
-    int getAmountOfFreeInventorySlots();
-
-    @Contract(pure = true)
-    default boolean hasFreeInventorySlot() {
-        return getAmountOfFreeInventorySlots() != 0;
-    }
-
-    @Nonnull
-    @Contract(pure = true)
-    PlayerLocation getLocation();
-
-    /**
-     * Creates a clickable message that executes the given callback, and is only clickable once (if selfCancelling is true).
-     *
-     * @param selfCancelling defines if the message is clickable multiple times
-     * @return new clickable message
-     */
-    @Nonnull
-    @Contract("_ -> new")
-    ClickableMessage createClickableMessage(boolean selfCancelling);
-
-    /**
-     * Creates a clickable message that executes the given callback, and is only clickable once (if selfCancelling is true).
-     *
-     * @param selfCancelling defines if the message is clickable multiple times
-     * @param callback       callback to be executed if player clicks the message
-     * @return new clickable message
-     */
-    @Nonnull
-    @Contract("_, _ -> new")
-    ClickableMessage createClickableMessage(boolean selfCancelling, @Nonnull Runnable callback);
 
     /**
      * Checks whether the user is in a virtual box whose span in each direction is twice the specified range.
---
+++ src/main/java/minevalley/core/api/users/ProxyUser.java
@@ -0,0 +1,260 @@
+package minevalley.core.api.users;
+
+import minevalley.core.api.audio.SoundReceiver;
+import minevalley.core.api.messaging.DialogReceiver;
+import minevalley.core.api.messaging.MessageReceiver;
+import minevalley.core.api.regions.utils.PlayerLocation;
+import minevalley.core.api.users.enums.Fraction;
+import minevalley.core.api.users.enums.McVersion;
+import minevalley.core.api.users.enums.TabListView;
+import minevalley.core.api.users.exceptions.UserNotPermittedException;
+import minevalley.core.api.utils.ChatHandler;
+import minevalley.core.api.utils.ClickableMessage;
+import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import java.time.Duration;
+
+@SuppressWarnings("unused")
+public interface ProxyUser extends User, DialogReceiver, MessageReceiver, SoundReceiver {
+
+    /**
+     * Gets the team-member object of this user.
+     *
+     * @return team-member object
+     * @throws UserNotPermittedException if the user is no team-member
+     * @see #isTeamler()
+     */
+    @Nonnull
+    TeamMember team() throws UserNotPermittedException;
+
+    /**
+     * Joins the team-service.
+     *
+     * @throws UnsupportedOperationException if the user is not allowed to join the team-service
+     * @throws IllegalStateException         if the user is already in a service
+     */
+    void joinTeamService() throws UnsupportedOperationException, IllegalStateException;
+
+    /**
+     * Gets if the player has any type of team-rank.
+     *
+     * @return true, if the user is part of the server-team (and in team-service)
+     */
+    @Contract(pure = true)
+    boolean isTeamler();
+
+    /**
+     * Gets whether this user is currently logged in via labymod.
+     * <br>
+     * <b>Note:</b> When joining, this boolean isn't set immediately. Make sure to use a delay, when using this method in a PlayerJoinListener!
+     *
+     * @return true, if this user is using labymod
+     */
+    @Contract(pure = true)
+    boolean isUsingLabyMod();
+
+    /**
+     * Gets the version of the client this user is using.
+     *
+     * @return client version
+     */
+    @Nonnull
+    @Contract(pure = true)
+    McVersion getVersion();
+
+    /**
+     * Sets the tab-list view of this user.
+     *
+     * @param view tab-list view to be set
+     * @throws IllegalArgumentException if view is null
+     */
+    void setTabListView(@Nonnull TabListView view) throws IllegalArgumentException;
+
+    /**
+     * Gets the current tab-list view of this user.
+     *
+     * @return current tab-list view
+     */
+    @Nonnull
+    @Contract(pure = true)
+    TabListView getTabListView();
+
+    /**
+     * Gets the user's fraction.
+     * Note: The user must be in fraction-service, otherwise this is NONE
+     *
+     * @return users fraction
+     */
+    @Contract(pure = true)
+    Fraction getFractionService();
+
+    /**
+     * Sets the user's current fraction service.
+     *
+     * @param service fraction to enter service
+     * @throws IllegalArgumentException  if the service is null
+     * @throws UserNotPermittedException if the user is not allowed to enter the service
+     * @throws IllegalStateException     if the user is already in a service
+     */
+    void enterFractionService(@Nonnull Fraction service) throws IllegalArgumentException, UserNotPermittedException,
+            IllegalStateException;
+
+    /**
+     * Lets the user leave the fraction-service.
+     *
+     * @throws IllegalStateException if the user is not in a service
+     */
+    void leaveFractionService() throws IllegalStateException;
+
+    /**
+     * Gets whether the user has reached the maximum idle time and is marked as afk.
+     *
+     * @return true, if the user reached the maximum idle time
+     */
+    @Contract(pure = true)
+    boolean isIdle();
+
+    /**
+     * Gets whether this user is vanished (only team members can be vanished).
+     *
+     * @return true, if this user is vanished
+     */
+    @Contract(pure = true)
+    boolean isVanish();
+
+    /**
+     * Sets whether this user is vanished.
+     *
+     * @param vanish true, if this user should be vanished
+     * @throws UnsupportedOperationException if the user is not allowed to vanish
+     */
+    void setVanish(boolean vanish) throws UnsupportedOperationException;
+
+    /**
+     * Revives the user if he's dead.
+     *
+     * @throws IllegalStateException if the user is not dead
+     */
+    void revive() throws IllegalStateException;
+
+    /**
+     * Imprisons this user for the given duration.
+     *
+     * @param duration duration of the imprisonment
+     * @throws UnsupportedOperationException if the user cannot be imprisoned (e.g. because he's a team-member)
+     * @throws IllegalStateException         if the user is already in prison
+     */
+    void imprison(@Nonnull Duration duration) throws UnsupportedOperationException, IllegalStateException;
+
+    /**
+     * Releases this user from prison.
+     *
+     * @throws IllegalStateException if the user is not in prison
+     */
+    void releaseFromPrison() throws IllegalStateException;
+
+    /**
+     * Gets whether the user has completed registration on our network.
+     *
+     * @return true, if the user is registered
+     */
+    @Contract(pure = true)
+    boolean isRegistered();
+
+    /**
+     * Clears the registration cache of this user and checks if he is now registered.
+     */
+    void checkRegistration();
+
+    /**
+     * Adds an item to the users inventory.
+     *
+     * @param item item to add
+     * @return true, if the item was added. False if the user does not have any empty slot.
+     * @throws IllegalArgumentException if the item is null
+     */
+    boolean addItem(@Nonnull ItemStack item) throws IllegalArgumentException;
+
+    /**
+     * Gets the amount of free slots in the users inventory (equipment slots not included).
+     *
+     * @return empty slots as integer
+     */
+    @Nonnegative
+    @Contract(pure = true)
+    int getAmountOfFreeInventorySlots();
+
+    /**
+     * Checks for any free slots in the users inventory (equipment slots not included)
+     *
+     * @return true, if the user has free slots
+     */
+    @Contract(pure = true)
+    default boolean hasFreeInventorySlot() {
+        return getAmountOfFreeInventorySlots() != 0;
+    }
+
+    /**
+     * Gets this users player location.
+     *
+     * @return player location
+     */
+    @Nonnull
+    @Contract(pure = true)
+    PlayerLocation getLocation();
+
+    /**
+     * Sends a message to the chat in the name of this user.
+     *
+     * @param type    type of the chat message
+     * @param message message to send
+     * @throws IllegalArgumentException if either the type or the message is null
+     */
+    default void chat(@Nonnull OnlineUser.ChatType type, @Nonnull String message) throws IllegalArgumentException {
+        if (type == null) throw new IllegalArgumentException("Type cannot be null");
+        ChatHandler.chat(this, type, message);
+    }
+
+    /**
+     * Sends a normal chat message in the name of this user.
+     *
+     * @param message message to send
+     * @throws IllegalArgumentException if either the type or the message is null
+     */
+    default void chat(@Nonnull String message) throws IllegalArgumentException {
+        if (message == null) throw new IllegalArgumentException("Message cannot be null");
+        ChatHandler.chat(this, OnlineUser.ChatType.NORMAL, message);
+    }
+
+    /**
+     * Performs a command in the name of the user
+     *
+     * @param command command with the slash
+     * @throws IllegalArgumentException if the command is null, or does not start with a slash
+     */
+    void performCommand(@Nonnull String command) throws IllegalArgumentException;
+
+    /**
+     * Creates a clickable message that executes the given callback, and is only clickable once (if selfCancelling is true).
+     *
+     * @param selfCancelling defines if the message is clickable multiple times
+     * @return new clickable message
+     */
+    @Nonnull
+    @Contract("_ -> new")
+    ClickableMessage createClickableMessage(boolean selfCancelling);
+
+    /**
+     * Creates a clickable message that executes the given callback, and is only clickable once (if selfCancelling is true).
+     *
+     * @param selfCancelling defines if the message is clickable multiple times
+     * @param callback       callback to be executed if player clicks the message
+     * @return new clickable message
+     */
+    @Nonnull
+    @Contract("_, _ -> new")
+    ClickableMessage createClickableMessage(boolean selfCancelling, @Nonnull Runnable callback);
+}
