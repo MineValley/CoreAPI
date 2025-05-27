2768c2d1b4a531bde8a2ebeff33d37bf035ffcf2
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-05-27 05:26:17 +0200

    add a lot to region class

--- src/main/java/minevalley/core/api/regions/Region.java
+++ src/main/java/minevalley/core/api/regions/Region.java
@@ -6,7 +6,11 @@ import minevalley.core.api.users.OnlineUser;
 import org.bukkit.Chunk;
 import org.bukkit.Location;
 
+import javax.annotation.Nonnegative;
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.List;
+import java.util.stream.Stream;
 
 @SuppressWarnings("unused")
 public interface Region {
@@ -16,39 +20,72 @@ public interface Region {
      *
      * @return id as integer
      */
+    @Nonnegative
     int getId();
 
     /**
-     * Gets a list of the areas that define this region.
+     * Gets a list of the areas that are included in the region.
      *
-     * @return array of areas
+     * @return list of included areas
      */
-    Area[] getIncluded();
-
-    Area[] getExcluded();
+    @Nonnull
+    List<Area> getIncluded();
 
     /**
-     * Gets an array with the chunks that this region lies on.
+     * Gets a list of the areas that are excluded by the region.
      *
-     * @return array of chunks
+     * @return list of excluded areas
      */
-    Chunk[] getChunks();
+    @Nonnull
+    List<Area> getExcluded();
 
-    List<OnlineUser> getUsersInRegion();
+    /**
+     * Gets a list with the chunks that this region lies in.
+     *
+     * @return list of chunks this region lies in
+     */
+    @Nonnull
+    List<Chunk> getChunks();
 
-    void kickUser(OnlineUser user, Location target);
+    /**
+     * Gets a stream of all the users that are currently in the region.
+     *
+     * @return stream of all users in the region
+     */
+    @Nonnull
+    Stream<OnlineUser> getUsersInRegion();
 
-    void kickAllUsers(Location target);
+    /**
+     * Kicks a user out of the region.
+     *
+     * @param user   user to kick
+     * @param target location to kick the user to
+     * @throws IllegalStateException    if the user is offline or not inside the region.
+     * @throws IllegalArgumentException if one of the arguments is null, or the target location is not in the same world as the region itself.
+     */
+    void kickUser(@Nonnull OnlineUser user, @Nonnull Location target) throws IllegalStateException, IllegalArgumentException;
+
+    /**
+     * Kicks all users out of this region.
+     * <p>
+     * <b>Note:</b> This
+     *
+     * @param target
+     */
+    default void kickAllUsers(@Nonnull Location target) {
+
+    }
 
     void setAllowEnter(boolean allow);
 
     boolean isAllowedToEnter();
 
     /**
-     * <b>Note:</b> This could be null!
+     * Gets the residence this region is used as, if existing.
      *
-     * @return the residence this region is connected to, if existing.
+     * @return the residence this region is used as, if existing.
      */
+    @Nullable
     Residence getResidence();
 
     void update(List<Area> included, List<Area> excluded);
