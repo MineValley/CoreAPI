0fbb0fc852a343b54ed6899a5ceba19c03b6af05
Thorben B. <thorbenbunger@gmail.com>
2025-07-02 10:29:12 +0200

    feat(DisplayRotationModifier): add yaw, pitch & roll logic

--- src/main/java/minevalley/core/api/modifiers/DisplayRotationModifier.java
+++ src/main/java/minevalley/core/api/modifiers/DisplayRotationModifier.java
@@ -4,6 +4,61 @@ package minevalley.core.api.modifiers;
 public interface DisplayRotationModifier {
 
     /**
+     * Set the rotation
+     *
+     * @param yaw   yaw rotation
+     * @param pitch pitch rotation
+     * @param roll  roll rotation
+     * @return this
+     */
+    DisplayRotationModifier setRotation(float yaw, float pitch, float roll);
+
+    /**
+     * Get yaw
+     *
+     * @return yaw
+     */
+    float getYaw();
+
+    /**
+     * Set yaw
+     *
+     * @param yaw the yaw to set
+     * @return this
+     */
+    DisplayRotationModifier setYaw(float yaw);
+
+    /**
+     * Get pitch
+     *
+     * @return pitch
+     */
+    float getPitch();
+
+    /**
+     * Set pitch
+     *
+     * @param pitch the pitch to set
+     * @return this
+     */
+    DisplayRotationModifier setPitch(float pitch);
+
+    /**
+     * Get roll
+     *
+     * @return roll
+     */
+    float getRoll();
+
+    /**
+     * Set roll
+     *
+     * @param roll the roll to set
+     * @return this
+     */
+    DisplayRotationModifier setRoll(float roll);
+
+    /**
      * Update the rotation
      */
     void updateRotation();
