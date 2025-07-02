eafd5db8c9113096f66180ed4b34f85a29a62f60
Thorben B. <thorbenbunger@gmail.com>
2025-07-02 10:25:54 +0200

    feat(DisplayRotationModifier): remove AxisAngle4f utilizing rotation methods

--- src/main/java/minevalley/core/api/modifiers/DisplayRotationModifier.java
+++ src/main/java/minevalley/core/api/modifiers/DisplayRotationModifier.java
@@ -1,25 +1,7 @@
 package minevalley.core.api.modifiers;
 
-import org.joml.AxisAngle4f;
-
 @SuppressWarnings("unused")
 public interface DisplayRotationModifier {
-
-    /**
-     * Set the rotation left
-     *
-     * @param leftRotation the rotation left
-     * @return the current instance
-     */
-    DisplayRotationModifier setLeftRotation(AxisAngle4f leftRotation);
-
-    /**
-     * Set the rotation right
-     *
-     * @param rightRotation the rotation right
-     * @return the current instance
-     */
-    DisplayRotationModifier setRightRotation(AxisAngle4f rightRotation);
 
     /**
      * Update the rotation
