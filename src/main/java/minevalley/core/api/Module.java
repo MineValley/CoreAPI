7d45a1c2bc98f708d91235dcaeb3706032b1ba5f
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2025-12-06 15:42:20 +0100

    chore: remove redundant fields in Module annotation

--- src/main/java/minevalley/core/api/Module.java
+++ src/main/java/minevalley/core/api/Module.java
@@ -1,20 +1,24 @@
 package minevalley.core.api;
 
-import minevalley.core.api.server.ServerType;
-
+import javax.annotation.Nonnull;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+/**
+ * Annotation to mark a class as a module.
+ * This automatically registers the module on startup.
+ */
 @Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.TYPE)
 @SuppressWarnings("unused")
 public @interface Module {
 
-    String name();
-
-    String[] authors();
-
-    ServerType[] servers() default {};
+    /**
+     * The name of the module.
+     *
+     * @return The module name.
+     */
+    @Nonnull String name();
 }
