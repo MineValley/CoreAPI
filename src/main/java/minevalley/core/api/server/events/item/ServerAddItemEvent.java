c67ae252ba8d2b3e61cbe4ccefc37527b0a01003
Thorben B. <thorbenbunger@gmail.com>
2025-12-22 12:37:44 +0100

    feat: add server item events

---
+++ src/main/java/minevalley/core/api/server/events/item/ServerAddItemEvent.java
@@ -0,0 +1,23 @@
+package minevalley.core.api.server.events.item;
+
+import lombok.Getter;
+import minevalley.core.api.server.events.ProxyServerEvents;
+import minevalley.core.api.users.ProxyUser;
+import org.bukkit.inventory.ItemStack;
+
+import javax.annotation.Nonnull;
+
+/**
+ * This event is called when an item is added to a user's inventory
+ */
+@SuppressWarnings("unused")
+public class ServerAddItemEvent extends ProxyServerEvents {
+
+    private final @Getter(onMethod_ = @Nonnull) ProxyUser user;
+    private final @Getter(onMethod_ = @Nonnull) ItemStack item;
+
+    public ServerAddItemEvent(@Nonnull ProxyUser user, @Nonnull ItemStack item) {
+        this.user = user;
+        this.item = item;
+    }
+}
