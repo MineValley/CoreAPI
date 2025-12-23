da71bed87b7149b780f37a1396e20b81bc4f8fc9
thorbenbuenger <thorbenbunger@gmail.com>
2025-12-23 17:24:45 +0100

    feat: add UserPreReceiveMessageEvent

---
+++ src/main/java/minevalley/core/api/users/events/messaging/UserPreReceiveMessageEvent.java
@@ -0,0 +1,22 @@
+package minevalley.core.api.users.events.messaging;
+
+import lombok.Getter;
+import minevalley.core.api.users.ProxyUser;
+import minevalley.core.api.users.events.ProxyUserEvent;
+
+import javax.annotation.Nonnull;
+import java.awt.*;
+
+/**
+ * This event is called when a message is to be sent to a user.
+ */
+@SuppressWarnings("unused")
+public class UserPreReceiveMessageEvent extends ProxyUserEvent {
+
+    private final @Getter(onMethod_ = @Nonnull) Component message;
+
+    public UserPreReceiveMessageEvent(@Nonnull ProxyUser user, @Nonnull Component message) {
+        super(user);
+        this.message = message;
+    }
+}
