b95f988f1b6407c53098c8511b9378c576e5cc81
thorbenbuenger <thorbenbunger@gmail.com>
2025-12-23 17:25:07 +0100

    feat: add UserPreReceiveActionBarEvent & UserPreReceiveTitleEvent

---
+++ src/main/java/minevalley/core/api/users/events/messaging/title/UserPreReceiveTitleEvent.java
@@ -0,0 +1,46 @@
+package minevalley.core.api.users.events.messaging.title;
+
+import lombok.Getter;
+import minevalley.core.api.users.ProxyUser;
+import minevalley.core.api.users.events.ProxyUserEvent;
+
+import javax.annotation.Nonnull;
+import java.awt.*;
+
+/**
+ * This event is called when a title is to be sent to a user.
+ */
+@SuppressWarnings("unused")
+public class UserPreReceiveTitleEvent extends ProxyUserEvent {
+
+    private final @Getter(onMethod_ = @Nonnull) Component title;
+    private final @Getter(onMethod_ = @Nonnull) Component subtitle;
+    private final long[] timings = {10, 70, 20};
+
+    public UserPreReceiveTitleEvent(@Nonnull ProxyUser proxyUser, @Nonnull Component title, @Nonnull Component subtitle) {
+        super(proxyUser);
+        this.title = title;
+        this.subtitle = subtitle;
+    }
+
+    public UserPreReceiveTitleEvent(@Nonnull ProxyUser proxyUser, @Nonnull Component title, @Nonnull Component subtitle, long fadeIn, long stay, long fadeOut) {
+        super(proxyUser);
+        this.title = title;
+        this.subtitle = subtitle;
+        this.timings[0] = fadeIn;
+        this.timings[1] = stay;
+        this.timings[2] = fadeOut;
+    }
+
+    public long getFadeIn() {
+        return timings[0];
+    }
+
+    public long getStay() {
+        return timings[1];
+    }
+
+    public long getFadeOut() {
+        return timings[2];
+    }
+}
