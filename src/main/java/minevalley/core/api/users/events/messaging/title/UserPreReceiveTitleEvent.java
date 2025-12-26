ff99dd216e54be228b8e8ada8f839bcae11be10c
thorbenbuenger <thorbenbunger@gmail.com>
2025-12-26 18:19:56 +0100

    feat(UserPreReceiveTitleEvent): rework timing

--- src/main/java/minevalley/core/api/users/events/messaging/title/UserPreReceiveTitleEvent.java
+++ src/main/java/minevalley/core/api/users/events/messaging/title/UserPreReceiveTitleEvent.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 import minevalley.core.api.users.ProxyUser;
 import minevalley.core.api.users.events.ProxyUserEvent;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.title.Title;
 
 import javax.annotation.Nonnull;
 
@@ -15,32 +16,19 @@ public class UserPreReceiveTitleEvent extends ProxyUserEvent {
 
     private final @Getter(onMethod_ = @Nonnull) Component title;
     private final @Getter(onMethod_ = @Nonnull) Component subtitle;
-    private final long[] timings = {10, 70, 20};
+    private final @Getter(onMethod_ = @Nonnull) Title.Times timings;
 
     public UserPreReceiveTitleEvent(@Nonnull ProxyUser proxyUser, @Nonnull Component title, @Nonnull Component subtitle) {
         super(proxyUser);
         this.title = title;
         this.subtitle = subtitle;
+        this.timings = Title.DEFAULT_TIMES;
     }
 
-    public UserPreReceiveTitleEvent(@Nonnull ProxyUser proxyUser, @Nonnull Component title, @Nonnull Component subtitle, long fadeIn, long stay, long fadeOut) {
+    public UserPreReceiveTitleEvent(@Nonnull ProxyUser proxyUser, @Nonnull Component title, @Nonnull Component subtitle, Title.Times timings) {
         super(proxyUser);
         this.title = title;
         this.subtitle = subtitle;
-        this.timings[0] = fadeIn;
-        this.timings[1] = stay;
-        this.timings[2] = fadeOut;
-    }
-
-    public long getFadeIn() {
-        return timings[0];
-    }
-
-    public long getStay() {
-        return timings[1];
-    }
-
-    public long getFadeOut() {
-        return timings[2];
+        this.timings = timings;
     }
 }
