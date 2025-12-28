3e7d9b9de812116cdda8f369f242d106a3d28641
Thorben B. <thorbenbunger@gmail.com>
2025-12-28 13:17:40 +0100

    feat: add & implement super ProxyEvent class

--- src/main/java/minevalley/core/api/server/events/ProxyServerEvents.java
+++ src/main/java/minevalley/core/api/server/events/ProxyServerEvents.java
@@ -1,17 +1,7 @@
 package minevalley.core.api.server.events;
 
-import lombok.Getter;
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
+import minevalley.core.api.events.ProxyEvent;
 
-import javax.annotation.Nonnull;
+public abstract class ProxyServerEvents extends ProxyEvent {
 
-public abstract class ProxyServerEvents extends Event {
-
-    private final static @Getter(onMethod_ = @Nonnull) HandlerList handlerList = new HandlerList();
-
-    @Override
-    public @Nonnull HandlerList getHandlers() {
-        return handlerList;
-    }
 }
--- src/main/java/minevalley/core/api/users/events/ProxyUserEvent.java
+++ src/main/java/minevalley/core/api/users/events/ProxyUserEvent.java
@@ -2,21 +2,13 @@ package minevalley.core.api.users.events;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import minevalley.core.api.events.ProxyEvent;
 import minevalley.core.api.users.ProxyUser;
-import org.bukkit.event.Event;
-import org.bukkit.event.HandlerList;
 
 import javax.annotation.Nonnull;
 
 @RequiredArgsConstructor
-public abstract class ProxyUserEvent extends Event {
+public abstract class ProxyUserEvent extends ProxyEvent {
 
     private final @Getter(onMethod_ = @Nonnull) ProxyUser user;
-
-    private final static @Getter(onMethod_ = @Nonnull) HandlerList handlerList = new HandlerList();
-
-    @Override
-    public @Nonnull HandlerList getHandlers() {
-        return handlerList;
-    }
 }
