65eec7e19dcc451d896139cd67a32c2e14e83c8e
Snabeldier <vincegamezspam@gmail.com>
2026-01-04 18:25:46 +0100

    docs(CustomArgumentTypes): add docs

--- src/main/java/minevalley/core/api/commands/CustomArgumentTypes.java
+++ src/main/java/minevalley/core/api/commands/CustomArgumentTypes.java
@@ -24,58 +24,130 @@ public final class CustomArgumentTypes {
     @Setter
     private static Manager manager;
 
+    /**
+     * Gets an argument type that resolves a single online user.
+     *
+     * @return the online user argument type
+     */
     public static @Nonnull ArgumentType<OnlineUser> onlineUser() {
         return manager.onlineUser();
     }
 
+    /**
+     * Gets an argument type that resolves multiple online users based on a selector.
+     *
+     * @return the online users selector argument type
+     */
     public static @Nonnull ArgumentType<OnlineUsersSelectorArgumentResolver> onlineUsers() {
         return manager.onlineUsers();
     }
 
+    /**
+     * Gets an argument type that resolves a group.
+     *
+     * @return the group argument type
+     */
     public static @Nonnull ArgumentType<Group> group() {
         return manager.group();
     }
 
+
+    /**
+     * Gets an argument type that resolves a fraction.
+     *
+     * @return the fraction argument type
+     */
     public static @Nonnull ArgumentType<Fraction> fraction() {
         return manager.fraction();
     }
 
+    /**
+     * Gets an argument type that resolves a team rank.
+     *
+     * @return the team rank argument type
+     */
     public static @Nonnull ArgumentType<TeamRank> teamRank() {
         return manager.teamRank();
     }
 
+    /**
+     * Gets an argument type that resolves a server type.
+     *
+     * @return the server type argument type
+     */
     public static @Nonnull ArgumentType<ServerType> server() {
         return manager.server();
     }
 
+    /**
+     * Gets an argument type that resolves a block selector.
+     *
+     * @return the block selector argument type
+     */
     public static @Nonnull ArgumentType<BlockSelectorArgumentResolver> block() {
         return manager.block();
     }
 
+    /**
+     * Gets an argument type that resolves a block state.
+     *
+     * @return the block state argument type
+     */
     public static @Nonnull ArgumentType<BlockState> blockState() {
         return ArgumentTypes.blockState();
     }
 
+    /**
+     * Gets an argument type that resolves an item stack.
+     *
+     * @return the item stack argument type
+     */
     public static @Nonnull ArgumentType<ItemStack> itemStack() {
         return ArgumentTypes.itemStack();
     }
 
+    /**
+     * Gets an argument type that resolves a named text color.
+     *
+     * @return the named text color argument type
+     */
     public static @Nonnull ArgumentType<NamedTextColor> namedColor() {
         return ArgumentTypes.namedColor();
     }
 
+    /**
+     * Gets an argument type that resolves a component.
+     *
+     * @return the component argument type
+     */
     public static @Nonnull ArgumentType<Component> component() {
         return ArgumentTypes.component();
     }
 
+    /**
+     * Gets an argument type that resolves a game mode.
+     *
+     * @return the game mode argument type
+     */
     public static @Nonnull ArgumentType<GameMode> gameMode() {
         return ArgumentTypes.gameMode();
     }
 
+    /**
+     * Gets an argument type that resolves a time in ticks.
+     *
+     * @return the time argument type
+     */
     public static @Nonnull ArgumentType<Integer> time() {
         return ArgumentTypes.time();
     }
 
+    /**
+     * Gets an argument type that resolves a time in ticks with a minimum time.
+     *
+     * @param minTime the minimum time in ticks
+     * @return the time argument type
+     */
     public static @Nonnull ArgumentType<Integer> time(final int minTime) {
         return ArgumentTypes.time(minTime);
     }
