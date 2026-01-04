19dd0744feab8931a7ed46c97b8d40d2f3e7e6ab
Snabeldier <vincegamezspam@gmail.com>
2026-01-04 18:16:06 +0100

    chore: rework RotationModifier

--- src/main/java/minevalley/core/api/armorstand/modifiers/RotationModifier.java
+++ src/main/java/minevalley/core/api/armorstand/modifiers/RotationModifier.java
@@ -1,21 +1,28 @@
 package minevalley.core.api.armorstand.modifiers;
 
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
+
 @SuppressWarnings("unused")
-public interface RotationModifier {
+public interface RotationModifier<T extends RotationModifier<T>> {
 
     /**
      * Sets the rotation of the armor stand
      *
      * @param rotation rotation in degrees
-     * @return the rotation modifier
+     * @return this
      */
-    RotationModifier setRotation(float rotation);
+    @Nonnull
+    @Contract("_ -> this")
+    T setRotation(float rotation);
 
     /**
      * Gets the rotation of the armor stand in degrees
      *
      * @return the rotation of the armor stand
      */
+    @Contract(pure = true)
     float getRotation();
 
     /**
