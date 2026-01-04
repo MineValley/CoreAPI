773977d4b955284baa28f25fe42ec542ab669411
Snabeldier <vincegamezspam@gmail.com>
2026-01-04 18:02:25 +0100

    docs(MetadataModifier): add docs

--- src/main/java/minevalley/core/api/armorstand/modifiers/MetadataModifier.java
+++ src/main/java/minevalley/core/api/armorstand/modifiers/MetadataModifier.java
@@ -1,131 +1,222 @@
 package minevalley.core.api.armorstand.modifiers;
 
+import minevalley.core.api.modifiers.VisibilityModifier;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
 @SuppressWarnings("unused")
 public interface MetadataModifier {
 
     /**
-     * @return true if the armorstand has a base plate
+     * Checks if the armor stand has a baseplate
+     *
+     * @return true if the armor stand has a baseplate
+     * @see #hasBaseplate()
      */
+    @Deprecated
+    @Contract(pure = true)
     boolean hasBasePlate();
 
     /**
-     * @param basePlate true if the armorstand should have a base plate
-     * @return the MetadataModifier
+     * Checks if the armor stand has a baseplate
+     *
+     * @return true if the armor stand has a baseplate
      */
-    MetadataModifier setBasePlate(boolean basePlate);
+    @Contract(pure = true)
+    boolean hasBaseplate();
 
     /**
-     * @return true if the armorstand is small
+     * Sets if the armor stand has a baseplate
+     *
+     * @param baseplate true if the armor stand has a baseplate
+     * @return this
      */
+    @Nonnull
+    @Contract("_ -> this")
+    MetadataModifier setBasePlate(boolean baseplate);
+
+    /**
+     * Checks if the armor stand is small
+     *
+     * @return true if the armor stand is small
+     */
+    @Contract(pure = true)
     boolean isSmall();
 
     /**
-     * @param small true if the armorstand should be small
+     * Sets if the armor stand is small
+     *
+     * @param small true if the armor stand should be small
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setSmall(boolean small);
 
     /**
-     * @return true if the armorstand has arms
+     * Checks if the armor stand has arms
+     *
+     * @return true if the armor stand has arms
      */
+    @Contract(pure = true)
     boolean hasArms();
 
     /**
-     * @param arms true if the armorstand should have arms
+     * @param arms true if the armor stand should have arms
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setArms(boolean arms);
 
     /**
-     * @return true if the armorstand is a marker
+     * Checks if the armor stand is a marker
+     *
+     * @return true if the armor stand is a marker
      */
     boolean isMarker();
 
     /**
-     * @param marker true if the armorstand should be a marker
+     * Sets if the armor stand is a marker
+     *
+     * @param marker true if the armor stand should be a marker
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setMarker(boolean marker);
 
     /**
-     * @return true if the armorstand is on fire
+     * Checks if the armor stand is on fire
+     * <p>
+     * <b>Note:</b> This only affects the visual effect, not actual fire damage. This can only be changed via {@link #setOnFire(boolean)} not by setting the 'entity' on fire.
+     *
+     * @return true if the armor stand is on fire
      */
+    @Contract(pure = true)
     boolean isOnFire();
 
     /**
-     * @param onFire true if the armorstand should be on fire
+     * Sets if the armor stand is on fire
+     *
+     * @param onFire true if the armor stand should be on fire
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setOnFire(boolean onFire);
 
     /**
-     * @return true if the armorstand is visible
+     * Checks if the armor stand is visible
+     * <p>
+     * <b>Note:</b> This only affects the visual effect, not actual visibility to players as in {@link VisibilityModifier}
+     *
+     * @return true if the armor stand is visible
      */
     boolean isVisible();
 
     /**
-     * @param visible true if the armorstand should be visible
+     * Sets if the armor stand is visible
+     * <p>
+     * <b>Note:</b> This only affects the visual effect, not actual visibility to players as in {@link VisibilityModifier}
+     *
+     * @param visible true if the armor stand should be visible
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setVisible(boolean visible);
 
     /**
-     * @return true if the armorstand is glowing
+     * Checks if the armor stand is glowing
+     *
+     * @return true if the armor stand is glowing
      */
     boolean isGlowing();
 
     /**
-     * @param glowing true if the armorstand should be glowing
+     * Sets if the armor stand is glowing
+     *
+     * @param glowing true if the armor stand should be glowing
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setGlowing(boolean glowing);
 
     /**
-     * @return true if the armorstand is affected by gravity
+     * Checks if the armor stand is affected by gravity
+     *
+     * @return true if the armor stand is affected by gravity
      */
     boolean hasGravity();
 
     /**
-     * @param gravity true if the armorstand should be affected by gravity
+     * Sets if the armor stand is affected by gravity
+     *
+     * @param gravity true if the armor stand should be affected by gravity
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setGravity(boolean gravity);
 
     /**
-     * @return custom name of the armorstand
+     * Gets the custom name of the armor stand, if existing
+     *
+     * @return custom name of the armor stand
      */
+    @Nullable
     String getCustomName();
 
     /**
-     * @param customName custom name of the armorstand
+     * Sets the custom name of the armor stand
+     *
+     * @param customName custom name of the armor stand
      * @return the MetadataModifier
      */
-    MetadataModifier setCustomName(String customName);
+    @Nonnull
+    @Contract("_ -> this")
+    MetadataModifier setCustomName(@Nullable String customName);
 
     /**
+     * Checks if the custom name is visible
+     *
      * @return true if the custom name is visible
      */
     boolean isCustomNameVisible();
 
     /**
+     * Sets if the custom name is visible
+     *
      * @param customNameVisible true if the custom name should be visible
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setCustomNameVisible(boolean customNameVisible);
 
     /**
-     * @return true if the armorstand is silent
+     * Checks if the armor stand is silent
+     *
+     * @return true if the armor stand is silent
      */
     boolean isSilent();
 
     /**
-     * @param silent true if the armorstand should be silent
+     * Sets if the armor stand is silent
+     *
+     * @param silent true if the armor stand should be silent
      * @return the MetadataModifier
      */
+    @Nonnull
+    @Contract("_ -> this")
     MetadataModifier setSilent(boolean silent);
 
     /**
-     * updates the metadata of the armorstand
+     * Updates the metadata of the armor stand
      */
     void updateMetadata();
 }
