7ca2081ba220bd5f1dae2dbc3ebe9e198c36f97b
Snabeldier <vincegamezspam@gmail.com>
2026-01-05 22:42:34 +0100

    chore(TranslationModifer): make TranslationModifier generic and fix wrong return type

--- src/main/java/minevalley/core/api/modifiers/TranslationModifier.java
+++ src/main/java/minevalley/core/api/modifiers/TranslationModifier.java
@@ -6,18 +6,18 @@ import javax.annotation.Nonnull;
 import java.util.Vector;
 
 @SuppressWarnings("unused")
-public interface TranslationModifier {
+public interface TranslationModifier<T extends TranslationModifier<T>> {
 
     /**
      * Set the translation
      *
      * @param translation the translation, default is (0, 0, 0)
-     * @return the current instance
+     * @return this
      * @throws IllegalArgumentException if the translation is null
      */
     @Nonnull
     @Contract("_ -> this")
-    ScaleModifier setTranslation(@Nonnull Vector<Float> translation) throws IllegalArgumentException;
+    T setTranslation(@Nonnull Vector<Float> translation) throws IllegalArgumentException;
 
     /**
      * Update the translation.
