02619674e59760b39877c451e5dafa4fbff9b6ed
Snabeldier <vincegamezspam@gmail.com>
2026-01-05 22:37:38 +0100

    chore(InteractionModifer): make InteractionModifier generic

--- src/main/java/minevalley/core/api/modifiers/InteractionModifier.java
+++ src/main/java/minevalley/core/api/modifiers/InteractionModifier.java
@@ -7,12 +7,13 @@ import javax.annotation.Nullable;
 import java.util.function.BiConsumer;
 
 @SuppressWarnings("unused")
-public interface InteractionModifier {
+public interface InteractionModifier<T extends InteractionModifier<T>> {
 
     /**
      * Defines the action when a user interacts with this object.
      *
      * @param consumer is called whenever a user interacts with this object
+     * @return this
      */
-    void onClick(@Nullable BiConsumer<OnlineUser, InteractionType> consumer);
+    T onClick(@Nullable BiConsumer<OnlineUser, InteractionType> consumer);
 }
