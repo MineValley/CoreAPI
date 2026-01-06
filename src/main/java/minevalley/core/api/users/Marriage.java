9e84abb3d1f6fccf5528ce145aaaea2513308b7e
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-01-06 18:13:33 +0100

    chore(Marriage): add nonnulls, contracts and exception throws

--- src/main/java/minevalley/core/api/users/Marriage.java
+++ src/main/java/minevalley/core/api/users/Marriage.java
@@ -1,15 +1,21 @@
 package minevalley.core.api.users;
 
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
 import java.util.Set;
 
 @SuppressWarnings("unused")
 public interface Marriage {
 
+    @Nonnull
+    @Contract(pure = true)
     Set<User> getUsers();
 
-    void add(User user);
+    void add(@Nonnull User user) throws IllegalArgumentException, IllegalStateException;
 
-    void leave(User user);
+    void leave(@Nonnull User user) throws IllegalArgumentException, IllegalStateException;
 
+    @Contract(pure = true)
     long since();
 }
