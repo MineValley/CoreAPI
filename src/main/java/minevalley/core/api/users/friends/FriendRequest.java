beffd843339b8efc7a9abb804e050e2d7047f9ea
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-01-06 18:11:40 +0100

    docs(FriendRequest): add javadoc

--- src/main/java/minevalley/core/api/users/friends/FriendRequest.java
+++ src/main/java/minevalley/core/api/users/friends/FriendRequest.java
@@ -8,18 +8,39 @@ import javax.annotation.Nonnull;
 @SuppressWarnings("unused")
 public interface FriendRequest {
 
+    /**
+     * Gets the user who sent the friend request.
+     *
+     * @return the sender of the friend request
+     */
     @Nonnull
     @Contract(pure = true)
     User getSender();
 
+    /**
+     * Gets the user who is the target of the friend request.
+     *
+     * @return the target of the friend request
+     */
     @Nonnull
     @Contract(pure = true)
     User getTarget();
 
+    /**
+     * Gets the timestamp when the friend request was sent.
+     *
+     * @return the sending timestamp in milliseconds since epoch
+     */
     @Contract(pure = true)
     long getSendingTimestamp();
 
+    /**
+     * Accepts the friend request, establishing a friendship between the sender and the target.
+     */
     void accept();
 
+    /**
+     * Rejects the friend request, declining the friendship offer.
+     */
     void reject();
 }
