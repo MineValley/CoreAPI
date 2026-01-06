6eb9fd0adfdc7757f75fb766223224281e6bce34
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-01-06 18:12:12 +0100

    docs(Friendship): add javadoc

--- src/main/java/minevalley/core/api/users/friends/Friendship.java
+++ src/main/java/minevalley/core/api/users/friends/Friendship.java
@@ -8,16 +8,34 @@ import javax.annotation.Nonnull;
 @SuppressWarnings("unused")
 public interface Friendship {
 
+    /**
+     * Gets the first friend in this friendship.
+     *
+     * @return the first friend
+     */
     @Nonnull
     @Contract(pure = true)
     User getFriend1();
 
+    /**
+     * Gets the second friend in this friendship.
+     *
+     * @return the second friend
+     */
     @Nonnull
     @Contract(pure = true)
     User getFriend2();
 
+    /**
+     * Gets the timestamp (in milliseconds since epoch) of when this friendship was created.
+     *
+     * @return the timestamp of when this friendship was created
+     */
     @Contract(pure = true)
     long since();
 
+    /**
+     * Ends this friendship.
+     */
     void exit();
 }
