0ae84a400299d94f961855e06d2baa5b13e23234
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-01-06 18:34:01 +0100

    docs(WeatherHandler): add javadoc, nonnulls and exception throws

--- src/main/java/minevalley/core/api/weather/WeatherHandler.java
+++ src/main/java/minevalley/core/api/weather/WeatherHandler.java
@@ -2,6 +2,9 @@ package minevalley.core.api.weather;
 
 import lombok.Setter;
 import org.bukkit.Chunk;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
 
 @SuppressWarnings("unused")
 public class WeatherHandler {
@@ -9,21 +12,51 @@ public class WeatherHandler {
     @Setter
     private static Manager manager;
 
+    /**
+     * Gets the current season.
+     *
+     * @return The current season.
+     */
+    @Nonnull
+    @Contract(pure = true)
     public static Season getCurrentSeason() {
         return manager.getCurrentSeason();
     }
 
-    public static Weather getCurrentWeather(Chunk chunk) {
+    /**
+     * Gets the current weather in the given chunk.
+     *
+     * @param chunk The chunk to get the weather for.
+     * @return The current weather in the given chunk.
+     */
+    @Nonnull
+    @Contract(pure = true)
+    public static Weather getCurrentWeather(@Nonnull Chunk chunk) {
         return manager.getWeather(System.currentTimeMillis(), chunk);
     }
 
-    public static Weather getUpcomingWeather(long timeMillis, Chunk chunk) {
+    /**
+     * Gets the upcoming weather in the given chunk at the specified time.
+     *
+     * @param timeMillis The time in milliseconds to get the weather for.
+     * @param chunk      The chunk to get the weather for.
+     * @return The upcoming weather in the given chunk at the specified time.
+     * @throws IllegalArgumentException If the chunk is null or the time is invalid.
+     */
+    @Nonnull
+    @Contract(pure = true)
+    public static Weather getWeather(long timeMillis, @Nonnull Chunk chunk) throws IllegalArgumentException {
         return manager.getWeather(System.currentTimeMillis(), chunk);
     }
 
     public interface Manager {
+
+        @Nonnull
+        @Contract(pure = true)
         Season getCurrentSeason();
 
-        Weather getWeather(long timestamp, Chunk chunk);
+        @Nonnull
+        @Contract(pure = true)
+        Weather getWeather(long timestamp, @Nonnull Chunk chunk) throws IllegalArgumentException;
     }
 }
