768cf369968f7d0bc7be6f78d63dbe6109aa0b25
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-01-06 18:10:34 +0100

    chore(FriendRequest): add nonnulls and contracts

--- src/main/java/minevalley/core/api/users/friends/FriendRequest.java
+++ src/main/java/minevalley/core/api/users/friends/FriendRequest.java
@@ -1,14 +1,22 @@
 package minevalley.core.api.users.friends;
 
 import minevalley.core.api.users.User;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
 
 @SuppressWarnings("unused")
 public interface FriendRequest {
 
+    @Nonnull
+    @Contract(pure = true)
     User getSender();
 
+    @Nonnull
+    @Contract(pure = true)
     User getTarget();
 
+    @Contract(pure = true)
     long getSendingTimestamp();
 
     void accept();
