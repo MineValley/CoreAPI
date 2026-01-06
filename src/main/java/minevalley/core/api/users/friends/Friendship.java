4def4ef937c53e97177d39911c414fec9c7c1aae
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-01-06 18:10:55 +0100

    chore(Friendship): add nonnulls and contracts

--- src/main/java/minevalley/core/api/users/friends/Friendship.java
+++ src/main/java/minevalley/core/api/users/friends/Friendship.java
@@ -1,14 +1,22 @@
 package minevalley.core.api.users.friends;
 
 import minevalley.core.api.users.User;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
 
 @SuppressWarnings("unused")
 public interface Friendship {
 
+    @Nonnull
+    @Contract(pure = true)
     User getFriend1();
 
+    @Nonnull
+    @Contract(pure = true)
     User getFriend2();
 
+    @Contract(pure = true)
     long since();
 
     void exit();
