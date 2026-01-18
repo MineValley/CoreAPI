61b7834eb957ec6d3a59ffec71a535ed4fe49d2d
thorbenbuenger <thorbenbunger@gmail.com>
2026-01-18 22:26:22 +0100

    feat(ScaleModifier): add non-vector scale updating

--- src/main/java/minevalley/core/api/modifiers/ScaleModifier.java
+++ src/main/java/minevalley/core/api/modifiers/ScaleModifier.java
@@ -11,6 +11,18 @@ public interface ScaleModifier<T extends ScaleModifier<T>> {
     /**
      * Set the scale
      *
+     * @param x  x scale
+     * @param y  y scale
+     * @param z  z scale
+     * @return the current instance
+     */
+    @Nonnull
+    @Contract("_, _, _ -> this")
+    T setScale(float x, float y, float z);
+
+    /**
+     * Set the scale
+     *
      * @param scale the scale, default is (x: 1.0, y: 1.0, z: 1.0)
      * @return the current instance
      * @throws IllegalArgumentException if the scale is null
