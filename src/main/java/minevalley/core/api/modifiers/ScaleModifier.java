484514503f0e8edb62772bc07017dba9f7839cd4
thorbenbuenger <thorbenbunger@gmail.com>
2026-01-18 22:21:09 +0100

    feat(ScaleModifier): change vector type

--- src/main/java/minevalley/core/api/modifiers/ScaleModifier.java
+++ src/main/java/minevalley/core/api/modifiers/ScaleModifier.java
@@ -1,9 +1,9 @@
 package minevalley.core.api.modifiers;
 
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Contract;
 
 import javax.annotation.Nonnull;
-import java.util.Vector;
 
 @SuppressWarnings("unused")
 public interface ScaleModifier<T extends ScaleModifier<T>> {
@@ -11,13 +11,13 @@ public interface ScaleModifier<T extends ScaleModifier<T>> {
     /**
      * Set the scale
      *
-     * @param scale the scale, default is (1.0, 1.0, 1.0)
+     * @param scale the scale, default is (x: 1.0, y: 1.0, z: 1.0)
      * @return the current instance
      * @throws IllegalArgumentException if the scale is null
      */
     @Nonnull
     @Contract("_ -> this")
-    T setScale(@Nonnull Vector<Float> scale) throws IllegalArgumentException;
+    T setScale(@Nonnull Vector scale) throws IllegalArgumentException;
 
     /**
      * Update the scale.
