3891efff02ce7ce2fa03a04a3cc6526ecf7994d7
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-03-01 17:26:28 +0100

    chore(TeamRank): remove useless integer

--- src/main/java/minevalley/core/api/user/enums/TeamRank.java
+++ src/main/java/minevalley/core/api/user/enums/TeamRank.java
@@ -4,8 +4,6 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
-import java.util.Arrays;
-
 @Getter
 @RequiredArgsConstructor(access = AccessLevel.PRIVATE)
 public enum TeamRank {
@@ -13,66 +11,50 @@ public enum TeamRank {
     /**
      * Users without any team-rank (or team members who aren't in team service).
      */
-    NONE(0, false),
+    NONE(false),
 
     /**
      * Members of the "Test-Support"-group.
      */
-    T_SUPPORT(1, true),
+    T_SUPPORT(true),
 
     /**
      * Members of the "Support"-group.
      */
-    SUPPORT(2, true),
+    SUPPORT(true),
 
     /**
      * Members of the "Moderation"-group.
      */
-    MODERATION(3, true),
+    MODERATION(true),
 
     /**
      * Members of the "Sonstiges"-group.
      */
-    MISCELLANEOUS(10, false),
+    MISCELLANEOUS(false),
 
     /**
      * Members of the "Bauteam"-group.
      */
-    BUILDING(11, false),
+    BUILDING(false),
 
     /**
      * Members of the "Entwicklung"-group.
      */
-    DEVELOPMENT(20, false),
+    DEVELOPMENT(false),
 
     /**
      * Members of the "Administration"-group.
      */
-    ADMINISTRATION(30, true),
+    ADMINISTRATION(true),
 
     /**
      * Members of the "Serverleitung"-group.
      */
-    SERVER_LEAD(100, true);
-
-    /**
-     * Identification-number of the rank, that is saved in the database.
-     */
-    private final int value;
+    SERVER_LEAD(true);
 
     /**
      * Boolean that says whether the teamler is in support-service by default when joining the server.
      */
     private final boolean supportOnJoin;
-
-    /**
-     * Gets the rank by giving the identification-number of the rank.
-     *
-     * @param value identification-number of the searched rank
-     * @return the matching team-rank
-     */
-    @SuppressWarnings("unused")
-    public static TeamRank getRank(int value) {
-        return Arrays.stream(values()).filter(teamRank -> teamRank.getValue() == value).findFirst().orElse(null);
-    }
 }
