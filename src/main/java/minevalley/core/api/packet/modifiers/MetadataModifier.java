87102a4ef5fae76cae36eed7df1ce204ed243321
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-03-01 16:24:12 +0100

    docs(MetadataModifier): add contracts

--- src/main/java/minevalley/core/api/packet/modifiers/MetadataModifier.java
+++ src/main/java/minevalley/core/api/packet/modifiers/MetadataModifier.java
@@ -14,6 +14,7 @@ public interface MetadataModifier<T extends MetadataModifier<T>> {
      *
      * @return true if the object is affected by gravity
      */
+    @Contract(pure = true)
     boolean hasGravity();
 
     /**
@@ -32,6 +33,7 @@ public interface MetadataModifier<T extends MetadataModifier<T>> {
      * @return custom name of the object
      */
     @Nullable
+    @Contract(pure = true)
     Component getCustomName();
 
     /**
@@ -49,6 +51,7 @@ public interface MetadataModifier<T extends MetadataModifier<T>> {
      *
      * @return true if the custom name is visible
      */
+    @Contract(pure = true)
     boolean isCustomNameVisible();
 
     /**
@@ -66,6 +69,7 @@ public interface MetadataModifier<T extends MetadataModifier<T>> {
      *
      * @return true if the armor stand is silent
      */
+    @Contract(pure = true)
     boolean isSilent();
 
     /**
