620687a66bf851d4e1ec7568f84c6b6224668b18
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-03-01 16:56:16 +0100

    refactor: move DebugType into type folder

--- src/main/java/minevalley/core/api/users/User.java
+++ src/main/java/minevalley/core/api/users/User.java
@@ -3,7 +3,7 @@ package minevalley.core.api.users;
 import minevalley.core.api.Depends;
 import minevalley.core.api.Registrant;
 import minevalley.core.api.corporation.Member;
-import minevalley.core.api.messaging.DebugType;
+import minevalley.core.api.messaging.type.DebugType;
 import minevalley.core.api.phone.Telephone;
 import minevalley.core.api.users.exceptions.UserNotOnlineException;
 import minevalley.core.api.users.exceptions.UserNotPermittedException;
