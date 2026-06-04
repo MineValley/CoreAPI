168b5db34700fe6ac05db3e9e8db7a519e29c6cb
Thorben B. <thorbenbunger@gmail.com>
2026-06-04 14:23:19 +0200

    feat(Interaction): remove unusable Modifiers & add Interaction specific fields

--- src/main/java/eu/minevalley/core/api/virtual/Interaction.java
+++ src/main/java/eu/minevalley/core/api/virtual/Interaction.java
@@ -1,8 +1,67 @@
 package eu.minevalley.core.api.virtual;
 
-import eu.minevalley.core.api.virtual.modifier.*;
+import eu.minevalley.core.api.virtual.modifier.InteractionModifier;
+import eu.minevalley.core.api.virtual.modifier.LocationModifier;
+import eu.minevalley.core.api.virtual.modifier.VisibilityModifier;
+import org.bukkit.Location;
+import org.jetbrains.annotations.Contract;
+
+import javax.annotation.Nonnull;
 
 public interface Interaction extends InteractionModifier<Interaction>, LocationModifier<Interaction>,
-        RotationModifier<Interaction>, ScaleModifier<Interaction>, VisibilityModifier<Interaction>,
-        TranslationModifier<Interaction> {
+        VisibilityModifier<Interaction> {
+
+    /**
+     * Get the width
+     *
+     * @return width
+     */
+    @Contract(pure = true)
+    float getWidth();
+
+    /**
+     * Set the width
+     *
+     * @param width the width, default is 1.0
+     * @return this
+     */
+    @Nonnull
+    @Contract(pure = true)
+    Interaction setWidth(float width);
+
+    /**
+     * Get the height
+     *
+     * @return height
+     */
+    @Contract(pure = true)
+    float getHeight();
+
+    /**
+     * Set the height
+     *
+     * @param height the height, default is 1.0
+     * @return this
+     */
+    @Nonnull
+    @Contract(pure = true)
+    Interaction setHeight(float height);
+
+    /**
+     * Update the dimensions
+     */
+    void updateDimensions();
+
+    /**
+     * Sets the location
+     * <br>
+     * <b>Note:</b> This does not ignore the yaw and pitch values.
+     *
+     * @param location new location.
+     * @return this
+     * @throws IllegalArgumentException if the location is null or invalid.
+     */
+    @Nonnull
+    @Contract("_ -> this")
+    Interaction setLocation(@Nonnull Location location) throws IllegalArgumentException;
 }
