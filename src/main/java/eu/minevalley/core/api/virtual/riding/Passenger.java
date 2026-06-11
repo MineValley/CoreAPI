9e82b5a54f62f6a0cf360921e624f66a877c60e7
Snabeldier <79211348+Snabeldier@users.noreply.github.com>
2026-06-11 18:47:54 +0200

    chore(Passenger): remove getName()

--- src/main/java/eu/minevalley/core/api/virtual/riding/Passenger.java
+++ src/main/java/eu/minevalley/core/api/virtual/riding/Passenger.java
@@ -2,22 +2,11 @@ package eu.minevalley.core.api.virtual.riding;
 
 import org.jetbrains.annotations.Contract;
 
-import javax.annotation.Nonnull;
-
 /**
  * Represents an entity that can act as a passenger of a {@link Vehicle}.
  */
 @SuppressWarnings("unused")
 public interface Passenger {
-
-    /**
-     * Gets the name of the passenger.
-     *
-     * @return name of the passenger
-     */
-    @Nonnull
-    @Contract(pure = true)
-    String getName();
 
     /**
      * Gets the entity id of the passenger.
